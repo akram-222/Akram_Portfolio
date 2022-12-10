d6fff37304de2edb388b0500c595f541caf5e0bd
Akram A. Abdelbasir <69124951+Ak-ram@users.noreply.github.com>
2022-12-10 21:26:17 +0000

    modified:   src/Layouts/Header/header.tsx

--- src/Layouts/Header/header.tsx
+++ src/Layouts/Header/header.tsx
@@ -5,8 +5,7 @@ function Header() {
     return (
         <header>
             <nav className='header-nav'>
-                <li><a href='#d' className='link link__color--effect'>C3 AI Decision Advantage</a></li>
-                <li><a href='#d' className='link link__color--effect'>Dashboard</a></li>
+                <li><a href='#d' className='link'>C3 AI Decision Advantage Dashboard</a></li>
             </nav>
         </header>
     );
