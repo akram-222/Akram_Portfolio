897abf85e575ce40dd981a33692366b937c8ed8a
Akram A. Abdelbasir <69124951+Ak-ram@users.noreply.github.com>
2022-12-10 10:46:52 +0000

    ADD: Grid Layout

---
+++ src/Components/Logo/logo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './logo.scss';
+
+function Logo() {
+    return (
+        <span className='logo' >
+            <img src="https://seeklogo.com/images/A/airplane-logo-6885950420-seeklogo.com.png" alt="logo"
+            />
+        </span>
+    );
+}
+
+export default Logo;
