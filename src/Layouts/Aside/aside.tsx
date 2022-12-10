bdde5b5e64cd5973c50112c3d1b28baf281c5682
Akram A. Abdelbasir <69124951+Ak-ram@users.noreply.github.com>
2022-12-10 16:27:43 +0000

    ADD: compass icon in aside

--- src/Layouts/Aside/aside.tsx
+++ src/Layouts/Aside/aside.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import './aside.scss';
 import { BsFillGrid1X2Fill, BsFillGearFill, BsFillCameraReelsFill, BsFillCloudDrizzleFill } from "react-icons/bs"
+// import { MousePostion } from "../../Utils/mousePositions"
+import { ImCompass2 } from 'react-icons/im'
 function Aside() {
+    const [coords, setCoords] = useState([0, 0])
+    useEffect(() => {
+        document.addEventListener('mousemove', (e) => {
+            setCoords([e.pageX, e.pageY]);
+            console.log(coords)
+            return coords
+        })
+    }, [coords])
     return (
         <aside>
             <ul>
                 <li>
+                    <a href='#d' className='link'><ImCompass2 size={20} style={{ transform: `rotate(${coords[0] - coords[1]}deg)` }} /></a>
+                </li>
+                <li>
                     <a href='#d' className='link'><BsFillGrid1X2Fill /></a>
                 </li>
+
                 <li>
                     <a href='#d' className='link'><BsFillCloudDrizzleFill /></a>
                 </li>
+
                 <li>
                     <a href='#d' className='link'><BsFillCameraReelsFill /></a>
                 </li>
