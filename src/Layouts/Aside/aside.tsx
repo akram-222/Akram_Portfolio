aea13bbf51c54a8bd75de80b4f5d38b1b2e02b5f
Akram A. Abdelbasir <69124951+Ak-ram@users.noreply.github.com>
2022-12-10 15:06:54 +0000

    ADD: aside more icons

--- src/Layouts/Aside/aside.tsx
+++ src/Layouts/Aside/aside.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import './aside.scss';
-import { BsFillGrid1X2Fill } from "react-icons/bs"
-import { TbAccessPoint } from "react-icons/tb"
-import { MdOutlineVerifiedUser } from "react-icons/md"
+import { BsFillGrid1X2Fill, BsFillGearFill, BsFillCameraReelsFill, BsFillCloudDrizzleFill } from "react-icons/bs"
 function Aside() {
     return (
         <aside>
             <ul>
                 <li><BsFillGrid1X2Fill /></li>
-                <li><TbAccessPoint /></li>
-                <li><MdOutlineVerifiedUser /></li>
-                <li><TbAccessPoint /></li>
-                <li><TbAccessPoint /></li>
-                
+                <li><BsFillCloudDrizzleFill /></li>
+                <li><BsFillCameraReelsFill /></li>
+                <li><BsFillGearFill /></li>                
             </ul>
         </aside>
     );
