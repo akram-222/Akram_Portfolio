6e677433f9c5902cb1d5c57bf9e9a63bdc97c996
Akram A. Abdelbasir <69124951+Ak-ram@users.noreply.github.com>
2022-12-11 09:03:14 +0000

    ADD: chart to TopKPI

--- src/Layouts/Main/subMainSections/TopKPIs/topKPIs.tsx
+++ src/Layouts/Main/subMainSections/TopKPIs/topKPIs.tsx
@@ -1,5 +1,6 @@
 import { AiOutlineInfoCircle } from "react-icons/ai"
 import { BsFillCaretDownFill } from "react-icons/bs"
+import { ChartView } from "../../../../Components/Charts/chartView";
 import "./topKPIs.scss";
 export const TopKPIs = () => {
     return (
@@ -10,7 +11,7 @@ export const TopKPIs = () => {
                     <h4 className="topKPI__header">High Risk Targets universitigated <AiOutlineInfoCircle size={15}/></h4>
                     <span className="topKPI__current">8</span>
                     <span className="topKPI__change"><BsFillCaretDownFill />5 (.5%)</span>
-                    <div className="topKPI__change--chart">chart</div>
+                    <div className="topKPI__change--chart"><ChartView /></div>
                     <span className="topKPI__timestamp">Last 30 days</span>
                 </div>
 
