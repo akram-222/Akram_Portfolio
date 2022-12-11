ca3c6e6d10ee02c0567a125a6acf6fffb8f926c9
Akram A. Abdelbasir <69124951+Ak-ram@users.noreply.github.com>
2022-12-11 08:10:03 +0000

    ADD: topKPI more content

--- src/Layouts/Main/subMainSections/TopKPIs/topKPIs.tsx
+++ src/Layouts/Main/subMainSections/TopKPIs/topKPIs.tsx
@@ -1,37 +1,37 @@
 import { AiOutlineInfoCircle } from "react-icons/ai"
-import "./topKPIs.scss"
+import "./topKPIs.scss";
 export const TopKPIs = () => {
     return (
         <section className="topKPIs__section">
             <h3>Top KPIs</h3>
             <div className="topKPIs__layout">
-                <div>
-                    <h4>High Risk Targets universitigated <AiOutlineInfoCircle/>></h4>
-                    <span>8</span>
-                    <span className="change">5 (.5%)</span>
-                    <div className="chart"></div>
-                    <span className="time">Last 30 days</span>
+                <div className="topKPI">
+                    <h4 className="topKPI__header">High Risk Targets universitigated <AiOutlineInfoCircle size={15}/></h4>
+                    <span className="topKPI__current">8</span>
+                    <span className="topKPI__change">5 (.5%)</span>
+                    <div className="topKPI___change--chart">chart</div>
+                    <span className="topKPI__timestamp">Last 30 days</span>
                 </div>
-                <div>
-                    <h4>High Risk Targets universitigated <AiOutlineInfoCircle/></h4>
-                    <span>8</span>
-                    <span className="change">5 (.5%)</span>
-                    <div className="chart"></div>
-                    <span className="time">Last 30 days</span>
+                <div className="topKPI">
+                    <h4 className="topKPI__header">High Risk Targets universitigated <AiOutlineInfoCircle size={15}/></h4>
+                    <span className="topKPI__current">8</span>
+                    <span className="topKPI__change">5 (.5%)</span>
+                    <div className="topKPI___change--chart">chart</div>
+                    <span className="topKPI__timestamp">Last 30 days</span>
                 </div>
-                <div className="topKPIs__layout--grow">
-                    <h4>High Risk Targets universitigated <AiOutlineInfoCircle/></h4>
-                    <span>8</span>
-                    <span className="change">5 (.5%)</span>
-                    <div className="chart"></div>
-                    <span className="time">Last 30 days</span>
+                <div className="topKPIs__layout--grow topKPI">
+                    <h4 className="topKPI__header">High Risk Targets universitigated <AiOutlineInfoCircle size={15}/></h4>
+                    <span className="topKPI__current">8</span>
+                    <span className="topKPI__change">5 (.5%)</span>
+                    <div className="topKPI___change--chart">chart</div>
+                    <span className="topKPI__timestamp">Last 30 days</span>
                 </div>
-                <div>
-                    <h4>High Risk Targets universitigated <AiOutlineInfoCircle/></h4>
-                    <span>8</span>
-                    <span className="change">5 (.5%)</span>
-                    <div className="chart"></div>
-                    <span className="time">Last 30 days</span>
+                <div className="topKPI">
+                    <h4 className="topKPI__header">High Risk Targets universitigated <AiOutlineInfoCircle size={15}/></h4>
+                    <span className="topKPI__current">8</span>
+                    <span className="topKPI__change">5 (.5%)</span>
+                    <div className="topKPI___change--chart">chart</div>
+                    <span className="topKPI__timestamp">Last 30 days</span>
                 </div>
             </div>
         </section>
