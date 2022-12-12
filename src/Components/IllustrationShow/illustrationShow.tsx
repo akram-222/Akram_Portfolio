3478980d0f57d15a4ea927c74be3d8a06a664f6f
Akram A. Abdelbasir <69124951+Ak-ram@users.noreply.github.com>
2022-12-12 09:35:11 +0000

    Modifiy: IllustrationShow Indicators

--- src/Components/IllustrationShow/illustrationShow.tsx
+++ src/Components/IllustrationShow/illustrationShow.tsx
@@ -5,6 +5,8 @@ export const IllustrationShow = () => {
         <div className="illustrationShow-container" style={{ backgroundImage: `url(${plane})` }}>
             <div className="indicators-container">
                 <span className="indicate indicate-1" data-info="this is tooltip 1">1</span>
+                <span className="indicate indicate-2" data-info="this is tooltip 1">2</span>
+                <span className="indicate indicate-3" data-info="this is tooltip 1">3</span>
             </div>
         </div>)
 }
