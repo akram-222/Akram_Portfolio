102ee164943494baaa0822c24f6694cd96d7b6ce
Akram A. Abdelbasir <69124951+Ak-ram@users.noreply.github.com>
2022-12-12 04:58:16 +0000

    ADD: 3D plane model to topKPI

--- src/Layouts/Main/subMainSections/TopKPIs/topKPIs.tsx
+++ src/Layouts/Main/subMainSections/TopKPIs/topKPIs.tsx
@@ -1,7 +1,6 @@
-import { AiOutlineInfoCircle } from "react-icons/ai"
-import { BsFillCaretDownFill } from "react-icons/bs"
+import { AiOutlineInfoCircle } from "react-icons/ai";
+import { BsFillCaretDownFill } from "react-icons/bs";
 import { ChartView } from "../../../../Components/Charts/chartView";
-import SensorAvailability from "../../../../Components/SensorAvailability/sensorAvailability";
 import "./topKPIs.scss";
 export const TopKPIs = () => {
     return (
@@ -9,27 +8,53 @@ export const TopKPIs = () => {
             <h3>Top KPIs</h3>
             <div className="topKPIs__layout">
                 <div className="topKPIs__layout--grow topKPI">
-                    <SensorAvailability />
+                    
+                        <iframe
+                            title="F-35A Lightning II"
+                            // frameorder="0"
+                            allowFullScreen
+                            // mozallowFullScreen="true"
+                            // webkitallowfullscreen="true"
+                            allow="autoplay; fullscreen; xr-spatial-tracking"
+                            xr-spatial-tracking
+                            execution-while-out-of-viewport
+                            execution-while-not-rendered
+                            web-share
+                            src="https://sketchfab.com/models/a06d6113cfb44a0aa7b8f17106aca9c4/embed?ui_theme=dark"
+                        >
+
+                        </iframe>{" "}
+                        
+                   
                 </div>
                 <div className="topKPI">
-                    <h4 className="topKPI__header">High Risk Targets universitigated <AiOutlineInfoCircle size={15}/></h4>
+                    <h4 className="topKPI__header">
+                        High Risk Targets universitigated <AiOutlineInfoCircle size={15} />
+                    </h4>
                     <span className="topKPI__current">8</span>
-                    <span className="topKPI__change"><BsFillCaretDownFill />5 (.5%)</span>
-                    <div className="topKPI__change--chart"><ChartView /></div>
+                    <span className="topKPI__change">
+                        <BsFillCaretDownFill />5 (.5%)
+                    </span>
+                    <div className="topKPI__change--chart">
+                        <ChartView />
+                    </div>
                     <span className="topKPI__timestamp">Last 30 days</span>
                 </div>
 
                 <div className=" topKPI">
-                    <h4 className="topKPI__header">High Risk Targets universitigated <AiOutlineInfoCircle size={15}/></h4>
+                    <h4 className="topKPI__header">
+                        High Risk Targets universitigated <AiOutlineInfoCircle size={15} />
+                    </h4>
                     <span className="topKPI__current">8</span>
-                    <span className="topKPI__change"><BsFillCaretDownFill />5 (.5%)</span>
-                    <div className="topKPI__change--chart"><ChartView /></div>
+                    <span className="topKPI__change">
+                        <BsFillCaretDownFill />5 (.5%)
+                    </span>
+                    <div className="topKPI__change--chart">
+                        <ChartView />
+                    </div>
                     <span className="topKPI__timestamp">Last 30 days</span>
                 </div>
- 
-                
             </div>
         </section>
-    )
-}
-
+    );
+};
