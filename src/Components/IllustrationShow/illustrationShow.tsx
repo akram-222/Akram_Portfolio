f8d078e66c000926e0895ea8e67bc5feea498d7e
Akram A. Abdelbasir <69124951+Ak-ram@users.noreply.github.com>
2022-12-12 11:31:22 +0000

    .

--- src/Components/IllustrationShow/illustrationShow.tsx
+++ src/Components/IllustrationShow/illustrationShow.tsx
@@ -1,19 +1,30 @@
 import "./illustrationShow.scss"
 import plane from "../../assests/plane.jpg"
+import { useRef } from "react"
 export const IllustrationShow = () => {
-    let expandIndicator = (e):void => {
-        let indicatorsContainer = e.target.closest('.indicators-container');
-        let indicators = indicatorsContainer.querySelectorAll('.indicator-expand');
-        [...indicators].map(indicator => indicator.classList.remove('indicator-expand'))
-        e.target.classList.toggle('indicator-expand');
-        console.log()
+    const indicatorsContainer = useRef<HTMLDivElement>(null);
+    let expandIndicator = (e): void => {
+        let indicators = indicatorsContainer.current!.querySelectorAll('.indicator-expand');
+        if (e.target.classList.contains('indicate')) {
+            [...indicators].map(indicator => indicator.classList.remove('indicator-expand'))
+            e.target.classList.add('indicator-expand')
+            e.target.innerHTML = e.target.dataset.info
+        }
+        [...indicators].map(
+            indicator: extends {dataset:} => {
+                indicator.classList.remove('indicator-expand')
+                indicator.innerHTML = indicator.dataset.number
+            }
+        )
+        // e.target.innerHTML = e.target.dataset.number
     }
+
     return (
         <div className="illustrationShow-container" style={{ backgroundImage: `url(${plane})` }}>
-            <div onClick={(e) => expandIndicator(e)} className="indicators-container">
-                <span  className="indicate indicate-1" data-info="this is tooltip 1">1</span>
-                <span className="indicate indicate-2" data-info="this is tooltip 1">2</span>
-                <span className="indicate indicate-3" data-info="this is tooltip 1">3</span>
+            <div onClick={(e) => expandIndicator(e)} className="indicators-container" ref={indicatorsContainer}>
+                <span className="indicate indicate-1" data-number='1' data-info="this is tooltip 1">1</span>
+                <span className="indicate indicate-2" data-number='2' data-info="this is tooltip 1">2</span>
+                <span className="indicate indicate-3" data-number='3' data-info="this is tooltip 1">3</span>
             </div>
         </div>)
 }
