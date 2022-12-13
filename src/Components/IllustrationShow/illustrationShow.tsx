9e0f30667fb776aa2293bcfafba58d589dd944c9
Akram A. Abdelbasir <69124951+Ak-ram@users.noreply.github.com>
2022-12-13 11:52:50 +0000

    Update: the tooltip

--- src/Components/IllustrationShow/illustrationShow.tsx
+++ src/Components/IllustrationShow/illustrationShow.tsx
@@ -1,49 +1,35 @@
 import "./illustrationShow.scss"
 import plane from "../../assests/plane.jpg"
-import { useRef } from "react"
-import { BiExpand } from "react-icons/bi"
 export const IllustrationShow = () => {
-    const indicatorsContainer = useRef<HTMLDivElement>(null);
-    let expandIndicator = (e): void => {
-        let indicators = indicatorsContainer.current!.querySelectorAll('.indicator-expand');
-        if (e.target.classList.contains('indicate')) {
-            if (e.target.classList.contains('indicator-expand')) return;
-            [...indicators].map(indicator => indicator.classList.remove('indicator-expand'))
-            e.target.classList.add('indicator-expand')
-            e.target.innerHTML = `
-           <section class="tooltip">
-            <h5 class="tooltip-header">
-                ${e.target.dataset.info}
-                <div class="tooltip-image-wrapper triangle">
-                <img class="tooltip-image " src="https://upload.wikimedia.org/wikipedia/commons/9/93/Tail_rotor_S-61.jpg" alt="" /></div>
-            </h5>
-            <div class="tooltip-info">
-            <p class="tooltip-description">The tail rotor is a smaller rotor mounted vertically or near-vertically at the tail of a traditional single-rotor helicopter</p>
-                <a class="tooltip-link" href="https://en.wikipedia.org/wiki/Tail_rotor">More Info</a>
-            </div>
-           </section>
-            
-            `
 
-        }
-        [...indicators].map(
-            (indicator) => {
-                indicator.classList.remove('indicator-expand')
-                // indicator.innerHTML = indicator.getAttribute('data-number')!
-                indicator.innerHTML = indicator.getAttribute('data-number')!
-                return undefined
-            }
-        )
-    }
+
 
     return (
         <div className="illustrationShow-container" style={{ backgroundImage: `url(${plane})` }}>
-            <div onClick={(e) => expandIndicator(e)} className="indicators-container" ref={indicatorsContainer}>
-                <span className="indicate indicate-1" data-number='1' data-info="Helicopter Tail Rotor">
-                    1
-                </span>
-                <span className="indicate indicate-2" data-number='2' data-info="this is tooltip 1">2</span>
-                <span className="indicate indicate-3" data-number='3' data-info="this is tooltip 1">3</span>
+            <div className="item-hints">
+                <div className="hint hint-1" data-position="4">
+                    <span className="hint-radius"></span>
+                    <span className="hint-dot"></span>
+                    <div className="hint-content do--split-children">
+                        <p>The tail rotor is a smaller rotor mounted vertically or near-vertically at the tail of a traditional single-rotor helicopter</p>
+                    </div>
+                </div>
+                <div className="hint  hint-2" data-position="4">
+                    <span className="hint-radius"></span>
+                    <span className="hint-dot"></span>
+                    <div className="hint-content do--split-children">
+                        <p>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+                    </div>
+                </div>
+                <div className="hint  hint-3" data-position="4">
+                    <span className="hint-radius"></span>
+                    <span className="hint-dot"></span>
+                    <div className="hint-content do--split-children">
+                        <p>The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+                    </div>
+                </div>
             </div>
-        </div>)
+
+        </div>
+    )
 }
