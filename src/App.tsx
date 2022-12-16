425fafe107da62270abbb37d76d9e4494f436587
Akram A. Abdelbasir <69124951+Ak-ram@users.noreply.github.com>
2022-12-16 00:40:41 +0000

    PUSH

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,38 @@
-// import './App.css';
+import './App.css';
 
-// import { useEffect, useState } from 'react';
-// import Sidebar from './Components/Sidebar';
-// import Content from './Components/Content';
-// import { githubAPI } from './Contexts/api';
-// const App = () => {
-//   const [showSidebar, onSetShowSidebar] = useState(false);
-//   const [repos, setRepos] = useState<any>()
-//   return (
-//     <div className="flex">
-//       <Sidebar
-//         onSidebarHide={() => {
-//           onSetShowSidebar(false);
-//         }}
-//         showSidebar={showSidebar}
-//       />
-//       <Content
-//         onSidebarHide={() => {
-//           onSetShowSidebar(true);
-//         }}
-//       />
-//     </div>
-//   );
-// }
-
-
-
-import React, { useEffect, useState } from 'react';
-
+import { useEffect, useState } from 'react';
+import Sidebar from './Components/Sidebar';
+import Content from './Components/Content';
 import { githubAPI } from './Contexts/api';
-
-
 const App = () => {
+  const [showSidebar, onSetShowSidebar] = useState(false);
   const [repos, setRepos] = useState<any>()
 
-
-
-
   useEffect(() => {
-
-    /*if the request is sent for the first time*/
-    githubAPI.getReposByName('ak-ram', 5, 1).then(res => {
+    githubAPI.getReposByName('ak-ram', 10, 1).then(res => {
       setRepos(res)
       console.log(repos)
-
-
-      console.log(repos)
     }).catch((reason) => {
-
+      new Error("error with githubAPI in App.tsx file")
     });
 
   }, [repos])
-
   return (
-    <div className="app">
-      <header>
-        <h1 className="app-title">App that search repos in Github by name</h1>
-      </header>
-
+    <div className="flex">
+      <Sidebar
+        onSidebarHide={() => {
+          onSetShowSidebar(false);
+        }}
+        showSidebar={showSidebar}
+      />
+      <Content
+        onSidebarHide={() => {
+          onSetShowSidebar(true);
+        }}
+      />
     </div>
-  )
+  );
 }
 
-export default App
 
+export default App
