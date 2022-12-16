7083ee8bedf89149cfb90697a94458b8bc99a045
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-16 13:41:32 +0000

    Update 6 files

--- src/Components/Repos.tsx
+++ src/Components/Repos.tsx
@@ -1,10 +1,9 @@
-import { useState } from "react";
 import { useOctokitResponse } from "../Contexts/OCTOKITAPIContext";
 import Icon from "./Icon";
 // import Image from "./Image";
 
 export default function Repos() {
-  const { gitHubData } = useOctokitResponse();
+  const {  gitHubData } = useOctokitResponse();
   console.log(gitHubData);
 
   return (
@@ -14,11 +13,11 @@ export default function Repos() {
         <Icon path="res-react-dash-plus" className="w-5 h-5" />
       </div>
       <div className="">
-        totals:{" "}
+        Result:{" "}
         <span className="text-sm text-premium-yellow">
           {gitHubData.isLoading
             ? "wait..."
-            : `${gitHubData?.items?.length} from ${gitHubData?.total_count}`}
+            : `${gitHubData?.items?.length} of ${gitHubData?.total_count}`}
         </span>
       </div>
       {gitHubData?.items?.map(
@@ -54,9 +53,9 @@ export default function Repos() {
 
       {/* <div className="flex-grow" /> */}
       <div className="flex justify-center mt-4">
-        <a
-          href="https://github.com/Ak-ram"
+        <button
           type="button"
+         
           className="text-white bg-[#050708] hover:bg-[#050708]/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#050708]/40 mr-2 mb-2"
         >
           GitHub Account
@@ -74,7 +73,7 @@ export default function Repos() {
               d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
             ></path>
           </svg>
-        </a>
+        </button>
       </div>
     </div>
   );
