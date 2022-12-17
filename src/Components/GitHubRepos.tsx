e1f349e36940b55236c88938021d6c806dfa4355
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-17 16:11:36 +0000

    Update package-lock.json and GitHubRepos.tsx

--- src/Components/GitHubRepos.tsx
+++ src/Components/GitHubRepos.tsx
@@ -1,6 +1,7 @@
 import { Octokit } from "@octokit/core";
 import { useEffect, useState } from "react";
-import { BsLink45Deg, BsPlusCircle, BsThreeDotsVertical } from "react-icons/bs"
+import { BsLink45Deg, BsPlusCircle, BsThreeDotsVertical } from "react-icons/bs";
+import { VscRepoForked } from "react-icons/vsc";
 import Icon from "./Icon";
 import Spinner from "./Spinner";
 // import Image from "./Image";
@@ -10,9 +11,10 @@ export default function Repos() {
   });
 
   const [repos, setRepos] = useState([]);
-  const [repoConfig, setRepoConfig] = useState({ per_page: 5 })
-  const [isLoad, setLoad] = useState(true)
-  const [isRepoDeleted, setIsDeleted] = useState(false)
+  const [repoConfig, setRepoConfig] = useState({ per_page: 5 });
+  const [isLoad, setLoad] = useState(true);
+  const [isRepoDeleted, setIsDeleted] = useState(false);
+  const [isHidden,setIsHidden] = useState(true)
   useEffect(() => {
     async function getRepos() {
       const { data } = await octokit.request(
@@ -21,12 +23,10 @@ export default function Repos() {
       );
 
       setRepos(data);
-      setLoad(false)
-
+      setLoad(false);
     }
     getRepos();
   }, [repoConfig, isRepoDeleted]);
-
 
   async function deleteRepo(event) {
     // await octokit.request(
@@ -36,61 +36,121 @@ export default function Repos() {
     //     repo: `${event.target.dataset.reponame}`
     //   }
     // );
-    console.log('event is fired', event.target.dataset.reponame)
+    console.log("event is fired", event.target.dataset.reponame);
   }
 
-  let UIReposList = repos?.map(({ size, name, has_issues, svn_url }, index: number) => (
-    <div className="flex items-center mt-3 relative show-repo-size" key={index}>
-      <div className="">{index + 1}</div>
+  let UIReposList = repos?.map(
+    ({ size, name, has_issues, svn_url }, index: number) => (
+      <div
+        className="flex items-center mt-3 relative show-repo-size"
+        key={index}
+      >
+        <div className="">{index + 1}</div>
 
-      {/* <Image path={`res-react-dash-flag-${index+1}`} className="ml-2 w-6 h-6" /> */}
-      <div className="ml-2 whitespace-nowrap text-ellipsis mr-3 w-28" >
-        {name}
-      </div>
-      <div className="flex-grow" />
-      <span className={`repo-size mr-2 text-sm whitespace-nowrap ${size > 2000 ? "text-red-500" : size < 500 ? "text-green-500" : "text-premium-yellow"}`}>{size} kb</span>
-      <div className="">
-        <a
-          className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-          href={svn_url}
+        {/* <Image path={`res-react-dash-flag-${index+1}`} className="ml-2 w-6 h-6" /> */}
+        <div className="ml-2 whitespace-nowrap text-ellipsis mr-3 w-28">
+          {name}
+        </div>
+        <div className="flex-grow" />
+        <span
+          className={`repo-size mr-2 text-sm whitespace-nowrap ${
+            size > 2000
+              ? "text-red-500"
+              : size < 500
+              ? "text-green-500"
+              : "text-premium-yellow"
+          }`}
         >
-          <BsLink45Deg />
-        </a>
-      </div>
-      <Icon
-        path={
-          has_issues
-            ? "res-react-dash-country-up"
-            : "res-react-dash-country-down"
-        }
-        className="w-4 h-4 mx-3"
-      />
-      <BsThreeDotsVertical className="w-2 h-2" />
-      {/* <button data-reponame={name} onClick={(event) => {
+          {size} kb
+        </span>
+        <div className="">
+          <a
+            className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+            href={svn_url}
+          >
+            <BsLink45Deg />
+          </a>
+        </div>
+        <Icon
+          path={
+            has_issues
+              ? "res-react-dash-country-up"
+              : "res-react-dash-country-down"
+          }
+          className="w-4 h-4 mx-3"
+        />
+        <BsThreeDotsVertical className="w-2 h-2" />
+        {/* <button data-reponame={name} onClick={(event) => {
         deleteRepo(event)
         setIsDeleted(true)
       }
       }>Del</button> */}
-    </div>
-  ))
-
+      </div>
+    )
+  );
 
   return (
     <div className="flex p-4 flex-col h-full overflow-y-auto relative">
       <div className="flex justify-between items-center">
         <div className="text-white font-bold">GitHub Repos</div>
-        <BsPlusCircle className="w-5 h-5" />
-        <div className="add-new-repo absolute top-5 bg-[#050708] p-4 rounded-xl text-center">
-          <label>
-            Repo name:<input type="text" />
-            <button>Create</button>
-          </label>
+        <BsPlusCircle className="w-5 h-5 cursor-pointer	" onClick={()=> setIsHidden(!isHidden)} />
+
+        <div className={`${isHidden? 'hidden':""} right-10 z-10 p-3 add-new-repo absolute top-5 bg-[#050708] rounded-lg`}>
+          <form>
+            <label
+              htmlFor="website-admin"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Repo name:
+            </label>
+            <label
+              htmlFor="search"
+              className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+            >
+              Search
+            </label>
+            <div className="relative">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-2 pointer-events-none">
+                <VscRepoForked className="text-green-500" />
+                {/* <span className="w-5 h-5 text-gray-500 dark:text-gray-400"> ak-ram/ </span> */}
+              </div>
+              <input
+                type="search"
+                id="search"
+                className="outline-none block w-full p-2 pl-8 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+                placeholder="new-repo"
+                required
+              />
+              <button
+                type="submit"
+                className="text-white absolute right-2.5 bottom-1 bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-3 py-1 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-blue-800"
+              >
+                New
+              </button>
+            </div>
+          </form>
+
+          {/* <label
+          htmlFor="website-admin"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Repo name:
+        </label>
+        <div className="flex">
+          <span className="inline-flex items-center px-1 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+            @
+          </span>
+          <input
+            type="text"
+            id="website-admin"
+            className="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 block flex-1 min-w-0 w-full text-sm border-gray-300 p-1  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+            placeholder="elonmusk"
+          />
+        </div> */}
         </div>
         {/* <Icon path="res-react-dash-plus" className="w-5 h-5" /> */}
       </div>
-      <div className="">
-        favourites
-      </div>
+      <div className="">favourites</div>
 
       {UIReposList}
 
@@ -99,13 +159,17 @@ export default function Repos() {
         <button
           type="button"
           onClick={() => {
-            setLoad(true)
-            setRepoConfig({ ...repoConfig, per_page: repoConfig.per_page + 5 })
+            setLoad(true);
+            setRepoConfig({ ...repoConfig, per_page: repoConfig.per_page + 5 });
             console.log(repos);
           }}
           className="text-white bg-[#050708] hover:bg-[#050708]/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#050708]/40 mr-2 mb-2"
         >
-          Total Repos:<span className="ml-1 w-10 text-sm text-premium-yellow"> {isLoad ? <Spinner /> : repos?.length}</span>
+          Total Repos:
+          <span className="ml-1 w-10 text-sm text-premium-yellow">
+            {" "}
+            {isLoad ? <Spinner /> : repos?.length}
+          </span>
         </button>
       </div>
     </div>
