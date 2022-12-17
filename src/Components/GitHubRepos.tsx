a6ff97334828c3bfe0a4596bd2b98dea68d27a67
Akram A. Abdelbasir <69124951+Ak-ram@users.noreply.github.com>
2022-12-17 09:34:35 +0000

    Fix: Fetching all data at the first rendering

--- src/Components/GitHubRepos.tsx
+++ src/Components/GitHubRepos.tsx
@@ -1,6 +1,7 @@
 import { Octokit } from "@octokit/core";
 import { useEffect, useState } from "react";
 import Icon from "./Icon";
+import Spinner from "./Spinner";
 // import Image from "./Image";
 export default function Repos() {
   const octokit = new Octokit({
@@ -9,17 +10,17 @@ export default function Repos() {
 
 
   const [repos, setRepos] = useState([]);
-  const [repoConfig, setRepoConfig] = useState({ per_page: 0 })
-  const [isLoad,setLoad] = useState(true)
+  const [repoConfig, setRepoConfig] = useState({ per_page: 5 })
+  const [isLoad, setLoad] = useState(true)
   useEffect(() => {
     async function getRepos() {
       const { data } = await octokit.request(
-        `GET /user/repos?per_page=${repoConfig.per_page}`,
+        `GET /user/repos?per_page=${repoConfig.per_page || 5}`, // Fix Issue
         {}
       );
+      console.log(repoConfig);
       setRepos(data);
       setLoad(false)
-      console.log(repos);
     }
     getRepos();
   }, [repoConfig]);
@@ -33,7 +34,7 @@ export default function Repos() {
         <Icon path="res-react-dash-plus" className="w-5 h-5" />
       </div>
       <div className="">
-       favourites
+        favourites
       </div>
 
       {repos?.map(({ name, has_issues, svn_url }, index: number) => (
@@ -66,7 +67,7 @@ export default function Repos() {
       ))}
 
       {/* <div className="flex-grow" /> */}
-      <div className="flex justify-center mt-4">
+      <div className="flex justify-center mt-4 relative">
         <button
           type="button"
           onClick={() => {
@@ -75,7 +76,7 @@ export default function Repos() {
           }}
           className="text-white bg-[#050708] hover:bg-[#050708]/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:hover:bg-[#050708]/40 mr-2 mb-2"
         >
-          Total Repos: <span className="ml-1 text-sm text-premium-yellow"> {isLoad ? "wait..." : repos?.length}</span>
+          Total Repos:<span className="ml-1 w-10 text-sm text-premium-yellow"> {isLoad ? <Spinner /> : repos?.length}</span>
         </button>
       </div>
     </div>
