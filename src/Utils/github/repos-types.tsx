003171e59c201ad1cfd0c61892189ad9a46a14f0
Akram A. Abdelbasir <69124951+Ak-ram@users.noreply.github.com>
2022-12-17 05:34:20 +0000

    new file:   src/Utils/github/repos-types.tsx

---
+++ src/Utils/github/repos-types.tsx
@@ -0,0 +1,8 @@
+export interface RepoConfigInterface {
+  per_page: number;
+  visibility: "all" | "private" | "public";
+  sort: "created" | "updated" | "pushed" | "full_name";
+  direction: "asc" | "desc";
+  page: number;
+  since: string;
+}
--- src/Utils/github/repos.tsx
+++ src/Utils/github/repos.tsx
@@ -2,20 +2,20 @@
 
 import { Octokit } from "@octokit/core";
 import { useEffect, useState } from "react";
+import { RepoConfigInterface } from "./repos-types";
 
 const octokit = new Octokit({
   auth: process.env.REACT_APP_GITHUB_ACCESS_TOKEN,
 });
-interface RepoConfigInterface {
-  per_page: number;
-  visibility: "all" | "private" | "public";
-  sort: "created" | "updated" | "pushed" | "full_name";
-  direction: "asc" | "desc";
-  page: number;
-  since: string;
-}
 
-const UserRepos = ({per_page,visibility,sort,since,direction,page}: RepoConfigInterface) => {
+const UserRepos = ({
+  per_page,
+  visibility,
+  sort,
+  since,
+  direction,
+  page,
+}: RepoConfigInterface) => {
   const [repos, setRepos] = useState([]);
   useEffect(() => {
     async function getRepos() {
