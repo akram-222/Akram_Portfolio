2e008b44d8015fe0e37a174c450e7ac45e52d1a6
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-18 07:03:34 +0000

    Update SegmentationData.tsx

--- src/Components/SegmentationData.tsx
+++ src/Components/SegmentationData.tsx
@@ -1,7 +1,7 @@
 export const segmentationData = [
+    { c1: 'JS / React', c2: '800', c3: '#b1a243', color: 'rgb(190 190 190)' },
     { c1: 'Typescript', c2: '441', c3: '#0074c3', color: 'rgb(170 170 170)' },
     { c1: 'Scss / Css', c2: '233', c3: '#2c365d', color: 'rgb(150 150 150)' },
-    { c1: 'JS / React', c2: '800', c3: '#b1a243', color: 'rgb(190 190 190)' },
     { c1: 'Html', c2: '126', c3: '#a73619', color: 'rgb(128 128 128)' },
 ];
 // export const segmentationData = [
