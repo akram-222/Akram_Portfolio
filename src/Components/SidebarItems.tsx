c7ed3d1c92302f180f61a5b3b89f2b48b2b38820
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-18 12:18:01 +0000

    Update SidebarIcons.tsx and SidebarItems.tsx

--- src/Components/SidebarItems.tsx
+++ src/Components/SidebarItems.tsx
@@ -2,14 +2,15 @@ const sidebarItems = [
     [
         { id: '0', title: 'Dashboard', notifications: false },
         { id: '1', title: 'Projects', notifications: false },
-        { id: '2', title: 'Blog', notifications: false },
-        { id: '3', title: 'Skills', notifications: false },
+        { id: '2', title: 'Notifications', notifications: 3 },
+        { id: '3', title: 'Blog', notifications: false },
     ],
     [
-        { id: '4', title: 'Status', notifications: false },
-        { id: '6', title: 'Notifications', notifications: 3 },
-        { id: '5', title: 'Reports', notifications: false },
-        { id: '6', title: 'Settings', notifications: false },
+        { id: '4', title: 'Skills', notifications: false },
+        { id: '5', title: 'Status', notifications: false },
+        { id: '6', title: 'Setting', notifications: false },
+        // { id: '7', title: 'Reports', notifications: false },
+        // { id: '8', title: 'Settings', notifications: false },
     ],
 ];
 
