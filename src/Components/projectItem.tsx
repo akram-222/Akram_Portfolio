1fd5cc8d9db9249c4f24f8fb301c5cc47ad318a8
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-18 20:13:18 +0000

    Update projectItem.tsx

--- src/Components/projectItem.tsx
+++ src/Components/projectItem.tsx
@@ -1,62 +1,20 @@
+import { AiOutlineFundProjectionScreen } from "react-icons/ai";
+import {  BsCalendarDate } from "react-icons/bs";
+import {HiOutlineLocationMarker} from "react-icons/hi"
 const ProjectItem = () => {
   return (
-    <div className="relative mr-auto w-full md:w-[45%] bg-white rounded-lg shadow-md px-4 dark:bg-[#171717] dark:border-gray-700 py-3 my-4">
+    <div className="relative mr-auto w-full md:w-[45%] bg-white rounded-lg shadow-md px-4 dark:bg-[#171717] dark:border-gray-700 py-3 mb-8">
       <div className=" text-white flex items-center absolute rounded-full p-3 shadow-xl bg-green-500 left-4 -top-6">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-8 w-8"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-          />
-        </svg>
+      <AiOutlineFundProjectionScreen size={30} />
       </div>
       <div className="mt-8">
         <p className="text-xl font-semibold my-2">Web Design</p>
         <div className="flex space-x-2 text-gray-400 text-sm">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-            />
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-            />
-          </svg>
+         <HiOutlineLocationMarker size={20}/>
           <p>Core UI Team</p>
         </div>
         <div className="flex space-x-2 text-gray-400 text-sm my-3">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-            />
-          </svg>
+          <BsCalendarDate size={18}/>
           <p>3 Weeks Left</p>
         </div>
         <div className="border-t-2 "></div>
