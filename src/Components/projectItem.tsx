3636fbaed75701b5c632571aba2a7ac8858bd218
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-18 21:52:11 +0000

    Update projectItem.tsx

--- src/Components/projectItem.tsx
+++ src/Components/projectItem.tsx
@@ -5,16 +5,24 @@ const ProjectItem = ({ name, createdAt, language, fork, visibility, i }) => {
       className={`border add-component-head relative mr-auto w-full md:w-[45%] bg-white rounded-lg shadow-md px-4 dark:bg-[#171717] dark:border-[#353535] py-3`}
     >
       {/* <div className="w-12 h-12 text-center flex justify-center text-white flex items-center absolute rounded-full p-3 shadow-xl bg-sidebar-card-top left-4 -top-6"> */}
-        {/* <AiOutlineFundProjectionScreen size={30} /> */}
-        {/* <span className="font-bold text-2xl">{i + 1}</span> */}
+      {/* <AiOutlineFundProjectionScreen size={30} /> */}
+      {/* <span className="font-bold text-2xl">{i + 1}</span> */}
       {/* </div> */}
       <div className="">
-     <div className=" flex justify-between items-center">
-         <span className="text-white text-xl font-semibold my-2 whitespace-nowrap overflow-hidden w-2/3 text-ellipsis">{i+1}. {name}</span>
-         <span className={`${visibility==="public"?"text-green-500":"text-rose-500"} border py-1 px-2 rounded-full text-sm border-[#353535]`}>{visibility}</span>
-     </div>
+        <div className=" flex justify-between items-center">
+          <span className="text-white text-xl font-semibold my-2 whitespace-nowrap overflow-hidden w-2/3 text-ellipsis">
+            {i + 1}. {name}
+          </span>
+          <span
+            className={`${
+              visibility === "public" ? "text-green-500" : "text-rose-500"
+            } border py-1 px-2 rounded-full text-sm border-[#353535]`}
+          >
+            {visibility}
+          </span>
+        </div>
         <div className="flex space-x-2 text-gray-400 text-sm">
-          <BsCodeSlash size={20}/>
+          <BsCodeSlash size={20} />
           <p>{fork ? "Fork" : language}</p>
         </div>
         <div className="flex space-x-2 text-gray-400 text-sm my-3">
