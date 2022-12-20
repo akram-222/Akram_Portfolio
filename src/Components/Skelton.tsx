22f7137c5addfc663fa49894f524212d3d0338e7
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-20 11:41:55 +0000

    Update Skelton.tsx

--- src/Components/Skelton.tsx
+++ src/Components/Skelton.tsx
@@ -3,9 +3,9 @@ import ProjectItem from "./projectItem";
 const Skelton = ({ className, number }) => {
   return (
     <>
-      {new Array(number).fill("").map((item,index) => (
+      {new Array(number).fill("").map((item, index) => (
         <div
-        key={index}
+          key={index}
           role="status"
           className={`${className} h-32 p-3 max-w-sm rounded border border-gray-200 shadow animate-pulse dark:border-gray-700`}
         >
@@ -14,24 +14,24 @@ const Skelton = ({ className, number }) => {
             <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32"></div>
             <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
             <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
-            
+
             {/* </div> */}
           </div>
           <div className="my-3 flex items-center space-x-2 w-full">
-              <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32"></div>
-              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
-              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
-            </div>
-            <div className="my-3 flex items-center space-x-2 w-full">
-              <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32"></div>
-              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
-              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
-            </div>
-            <div className="my-3 flex items-center space-x-2 w-full">
-              <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32"></div>
-              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
-              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
-            </div>
+            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          </div>
+          <div className="my-3 flex items-center space-x-2 w-full">
+            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          </div>
+          <div className="my-3 flex items-center space-x-2 w-full">
+            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24"></div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-full"></div>
+          </div>
         </div>
       ))}
     </>
