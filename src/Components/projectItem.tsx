65b98aef2e634cf2676ac1700889a099593d6ec7
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-20 19:05:50 +0000

    Update 2 files

--- src/Components/projectItem.tsx
+++ src/Components/projectItem.tsx
@@ -1,4 +1,5 @@
 import { BsCalendarDate, BsCodeSlash } from "react-icons/bs";
+import Badge from "./Badge";
 const ProjectItem = ({ name, createdAt, language, fork, visibility, i,repoConfig }) => {
 
   const indexingSys = ((i + 1) + (repoConfig.per_page * (repoConfig.page-1))) || (i+1)
@@ -18,13 +19,10 @@ const ProjectItem = ({ name, createdAt, language, fork, visibility, i,repoConfig
           <span className="text-white font-semibold my-2 whitespace-nowrap overflow-hidden w-2/3 text-ellipsis">
             {indexingSys}. {name.replace('-'," ")}
           </span>
-          <span
-            className={`${
-              visibility === "public" ? "text-green-500" : "text-rose-500"
-            } border py-1 px-2 rounded-full text-sm border-[#353535]`}
-          >
-            {visibility}
-          </span>
+          
+            {/* {visibility} */}
+            <Badge visibility ={visibility} />
+          
         </div>
         <div className="flex space-x-2 text-gray-400 text-sm">
           <BsCodeSlash size={20} />
