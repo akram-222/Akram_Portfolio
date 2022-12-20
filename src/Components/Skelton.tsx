9ac4efafef371bafecae47efbecf69e5e9e851ff
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-20 20:43:24 +0000

    Update Skelton.tsx

--- src/Components/Skelton.tsx
+++ src/Components/Skelton.tsx
@@ -7,7 +7,7 @@ const Skelton = ({ className, number }) => {
         <div
           key={index}
           role="status"
-          className={`${className} flex flex-col space-y-2 h-32 p-3 max-w-sm rounded border border-gray-200 shadow animate-pulse dark:border-gray-700`}
+          className={`${className} flex flex-col space-y-2 h-[130.43px] p-3 max-w-sm rounded border border-gray-200 shadow animate-pulse dark:border-gray-700`}
         >
           <div className="my-2 flex items-center space-x-2 w-full ">
             <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-full"></div>
