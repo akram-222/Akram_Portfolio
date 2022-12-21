5136cf48cefbfa79394eca0d95fcc90b6d7bc8e1
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-21 14:36:05 +0000

    Update 2 files

--- src/Components/Filter.tsx
+++ src/Components/Filter.tsx
@@ -1,4 +1,5 @@
-import {FaFilter} from 'react-icons/fa'
+import {FaFilter} from 'react-icons/fa';
+import RadioItem from "./RadioItem"
 const Filter=()=> {
     return (
         <> 
@@ -9,11 +10,18 @@ const Filter=()=> {
                 </h3>
         </header>
         <div className="mt-3 ">
-            <div className="mb-5">
-    <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">By project name:</label>
-    <input placeholder="ex; amazon" type="search" id="small-input" className="block w-full p-1.5 text-gray-900 border border-gray-300 rounded bg-gray-50 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
-    </div>
-    <div className='divider border-b dark:border-[#353535]'></div>
+            <div className="">
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">By project name:</label>
+                <input placeholder="ex; amazon" type="search" id="small-input" className="block w-full p-1.5 text-gray-900 border border-gray-300 rounded bg-gray-50 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+            </div>
+            <div className='divider border-b my-4 dark:border-[#353535]'></div>
+            <div className="">
+                <span className="block text-sm font-medium text-gray-900 dark:text-white"> Language</span>
+                <div className="languages mt-2">
+                    <RadioItem language="JavaScript" hint="React | Angular | Vue"/>
+                    <RadioItem language="TypeScript" hint=""/>
+                </div>
+            </div> 
         </div>
         </>
     )
