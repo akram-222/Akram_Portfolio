ac295a2bb3620e01323ca1b9a3badcd664bfb8a1
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-22 16:16:38 +0000

    Update Calendar.tsx

--- src/Components/Calendar.tsx
+++ src/Components/Calendar.tsx
@@ -1,10 +1,21 @@
+import { useEffect, useState } from "react";
 import { BiChevronRight, BiChevronLeft } from "react-icons/bi";
 const Calendar = ({ isHidden }) => {
+  const [timeTick, setTimeTick] = useState(
+    new Date().toLocaleTimeString("en-US", {})
+  );
   const weekdays = ["Sat", "Sun", "Mon", "Tue", "Wed", "Thr", "Fri"];
   const monthDays: number[] = []; // Fix Issue => What is "not assignable to parameter of type never" error in TypeScript?
   for (let i: number = 1; i <= 30; i++) {
     monthDays.push(i);
   }
+
+  useEffect(() => {
+    setInterval(() => {
+      setTimeTick(new Date().toLocaleTimeString("en-US", {}));
+    }, 1000);
+  }, []);
+
   return (
     <>
       <div
@@ -45,16 +56,17 @@ const Calendar = ({ isHidden }) => {
                 );
               })}
 
-              {["", "", "", ...monthDays].map((day) => {
+              {["", "", "", ...monthDays].map((day,i) => {
                 return (
                   <div
+                  key={`${day}__${i}`}
                     className={`${
                       day.toString().length
                         ? "dark:hover:bg-[#050708]/40  cursor-pointer dark:hover:text-blue-400"
                         : ""
                     } ${
                       day === new Date().getDate()
-                        ? "dark:bg-[#050708]/40 dark:text-rose-400 dark:hover:text-rose-400 font-semibold"
+                        ? "dark:bg-[#050708]/40 dark:text-rose-400 dark:hover:text-rose-400 animate-pulse"
                         : ""
                     } rounded w-8 h-8 flex items-center justify-center`}
                   >
@@ -67,18 +79,18 @@ const Calendar = ({ isHidden }) => {
           <div className="p-5 dark:bg-gray-700 bg-gray-50 rounded-b">
             <div className="px-4">
               <div className="border-b pb-4 border-gray-400 border-dashed">
-                <p className="text-xs font-light leading-3 text-gray-500 dark:text-gray-300">
-                  9:00 AM
+                <p className="mb-2 text-xs font-light leading-3 text-gray-500 dark:text-gray-300">
+                  {timeTick}
                 </p>
                 <a
                   href="#d"
                   tabIndex={0}
                   className="focus:outline-none text-lg font-medium leading-5 text-gray-800 dark:text-gray-100 mt-2"
                 >
-                  Zoom call with design team
+                  Fetched Repos will be
                 </a>
                 <p className="text-sm pt-2 leading-4 leading-none text-gray-600 dark:text-gray-300">
-                  Discussion on UX sprint and Wireframe review
+                  All Repos which created from 1/1/2022 to 1/1/2023
                 </p>
               </div>
             </div>
