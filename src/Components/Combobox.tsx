45fa0b08a2bafc6a393b5502702aad7c5f0ba3eb
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-22 11:15:31 +0000

    Update Combobox.tsx and Dashboard.tsx

--- src/Components/Combobox.tsx
+++ src/Components/Combobox.tsx
@@ -1,54 +1,77 @@
-import {useState,useEffect} from 'react'
-import {IoIosClose,IoIosArrowDown} from "react-icons/io"
-const Combobox = ()=>{
-    const [isCollapsed,setCollapsing]=useState(true)
-    const [value,setValue]=useState<string>('');
-    const handleSearchValue = (e)=> {
-          const target = e.target as HTMLTextAreaElement;
-          setValue(target.value);
-          setCollapsing(false)
-    }
-    const resetSearchValue = (e)=>{
-      setValue("")
-    }
-    return(
-        <div className="">
-  <div className="max-w-md mx-auto">
-    <label htmlFor="select" className="block py-2">Select Language:</label>
-
-    <div className="relative">
-      <div className="dark:bg-gray-800 h-10 bg-white flex rounded items-center">
-        <input onInput={handleSearchValue} value={value} name="select" id="select" placeholder="Choose Language" className="dark:bg-gray-800 px-1.5 appearance-none outline-none text-gray-200 w-full"  />
-        <button onClick={resetSearchValue} className="border-r dark:border-gray-700 cursor-pointer outline-none focus:outline-none transition-all text-gray-300 hover:text-gray-600">
-          <IoIosClose size={25} />
-        </button>
-        <label onClick={()=> setCollapsing(!isCollapsed)} 
-        htmlFor="show_more" 
-        className="cursor-pointer outline-none focus:outline-none  border-gray-200 transition-all text-gray-300 hover:text-gray-600 mr-2 ml-1">
-          <IoIosArrowDown size={18}/>
+import { useState } from "react";
+import { IoIosClose, IoIosArrowDown } from "react-icons/io";
+const Combobox = () => {
+  const [isCollapsed, setCollapsing] = useState(true);
+  const [value, setValue] = useState<string>("");
+  const handleSearchValue = (e) => {
+    const target = e.target as HTMLTextAreaElement;
+    setValue(target.value);
+    setCollapsing(false);
+  };
+  const resetSearchValue = () => {
+    setValue("");
+  };
+  return (
+    <div className="">
+      <div className="max-w-md mx-auto">
+        <label htmlFor="select" className="block py-2">
+          Select Language:
         </label>
-      </div>
 
-      <input type="checkbox" name="show_more" id="show_more" className="hidden" />
-      <div className={`${isCollapsed?"hidden" :""} absolute rounded shadow bg-white overflow-hidden flex-col w-full mt-1 border border-gray-200`}>
-        <div className="cursor-pointer group" onClick={()=> setCollapsing(true)}>
-          <a href="#d" className="block p-2 border-transparent border-l-4 group-hover:border-blue-600 group-hover:bg-gray-100">Python</a>
-        </div>
-        <div className="cursor-pointer group border-t" onClick={()=> setCollapsing(true)}>
-          <a href="#d" className="block p-2 border-transparent border-l-4 group-hover:border-blue-600 border-blue-600 group-hover:bg-gray-100">Javascript</a>
-        </div>
-        <div className="cursor-pointer group border-t" onClick={()=> setCollapsing(true)}>
-          <a href="#d" className="block p-2 border-transparent border-l-4 group-hover:border-blue-600 group-hover:bg-gray-100">Node</a>
-        </div>
-        <div className="cursor-pointer group border-t" onClick={()=> setCollapsing(true)}>
-          <a href="#d" className="block p-2 border-transparent border-l-4 group-hover:border-blue-600 group-hover:bg-gray-100">PHP</a>
+        <div className="relative">
+          <div className="dark:bg-gray-800 h-10 bg-white flex rounded items-center">
+            <input
+              onInput={handleSearchValue}
+              value={value}
+              name="select"
+              id="select"
+              placeholder="Choose Language"
+              className="dark:bg-gray-800 px-1.5 appearance-none outline-none text-gray-200 w-full"
+            />
+            <button
+              onClick={resetSearchValue}
+              className="border-r dark:border-gray-700 cursor-pointer outline-none focus:outline-none transition-all text-gray-300 hover:text-rose-400"
+            >
+              <IoIosClose size={25} />
+            </button>
+            <label
+              onClick={() => setCollapsing(!isCollapsed)}
+              htmlFor="show_more"
+              className="cursor-pointer outline-none focus:outline-none  border-gray-200 transition-all text-gray-300 hover:text-gray-400 mr-2 ml-1"
+            >
+              <IoIosArrowDown size={18} />
+            </label>
+          </div>
+
+          <input
+            type="checkbox"
+            name="show_more"
+            id="show_more"
+            className="hidden"
+          />
+          <div
+            className={`${
+              isCollapsed ? "hidden" : ""
+            } absolute dark:border-[#353535] rounded shadow bg-white overflow-hidden flex-col w-full mt-1 border border-gray-200`}
+          >
+            {["Python", "JavaScript", "PHP", "Rust"].map((language) => {
+              return (
+                <div
+                  className="cursor-pointer group block border-l-4 dark:bg-card dark:border-b dark:border-b-[#353535] border-l-transparent dark:hover:text-blue-400 hover:border-blue-600 p-2"
+                  onClick={() => {
+                    setCollapsing(true);
+                    // handleSearchValue(e)
+                  }}
+                >
+                  {language}
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
+  );
+};
 
-    )
-}
-
-export default Combobox
+export default Combobox;
