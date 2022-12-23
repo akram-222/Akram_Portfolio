d68c12c6d63a8ab56e893d9d8813f2f21745ed7a
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-23 12:20:24 +0000

    ✨ Feat: Add Emoji Component

--- src/Components/Filter.tsx
+++ src/Components/Filter.tsx
@@ -7,19 +7,19 @@ import Calendar from "./Calendar";
 import { BsChevronExpand } from "react-icons/bs";
 const Filter = () => {
   const [isHidden, setIsHidden] = useState(true);
-  const [isEmojiComponentHidden, SetIsEmojiComponentHidden] = useState(true);
+  const [isEmojiComponentHidden, setIsEmojiComponentHidden] = useState(true);
   const [isCollapsed, setCollapsed] = useState(true);
-  const showEmojiList = (event: React.SyntheticEvent) => {
-    let divTextContent = event.currentTarget as HTMLInputElement;
-    if (
-      divTextContent.textContent!.length === 1 &&
-      divTextContent.textContent!.charAt(0) === ":"
-    ) {
-      SetIsEmojiComponentHidden(false);
-    } else {
-      SetIsEmojiComponentHidden(true);
-    }
-  };
+  // const showEmojiList = (event: React.SyntheticEvent) => {
+  //   let divTextContent = event.currentTarget as HTMLInputElement;
+  //   if (
+  //     divTextContent.textContent!.length === 1 &&
+  //     divTextContent.textContent!.charAt(0) === ":"
+  //   ) {
+  //     SetIsEmojiComponentHidden(false);
+  //   } else {
+  //     SetIsEmojiComponentHidden(true);
+  //   }
+  // };
   return (
     <div
       className={`${
@@ -42,13 +42,16 @@ const Filter = () => {
         <div className="my-3 relative">
           <span className="block mb-2 font-medium">By project name:</span>
 
-          <div
+          {/* <div
             onInput={(e) => showEmojiList(e)}
             contentEditable={true}
             className=" h-10 flex items-center block w-full p-1.5 dark:text-gray-500 font-semibold border border-gray-300 rounded bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="⭐️ Type : to add emojis"
-          ></div>
-          <EmojiComponent isEmojiComponentHidden={isEmojiComponentHidden} />
+          ></div> */}
+          <EmojiComponent
+            setIsEmojiComponentHidden={setIsEmojiComponentHidden}
+            isEmojiComponentHidden={isEmojiComponentHidden}
+          />
         </div>
         <div className="my-3">
           <span className="block pb-1">Select Language:</span>
--- src/Components/emojiComponent.tsx
+++ src/Components/emojiComponent.tsx
@@ -1,11 +1,25 @@
 import { useEffect, useState, useRef } from "react";
 import { __getEmojiList } from "../Utils/github/__gitEmojiList";
-const EmojiComponent = ({ isEmojiComponentHidden }) => {
+const EmojiComponent = ({
+  isEmojiComponentHidden,
+  setIsEmojiComponentHidden,
+}) => {
   const [emojis, setEmojis] = useState({});
   const inputFeild = useRef<HTMLDivElement | null>(null);
   useEffect(() => {
     __getEmojiList().then((mojis) => setEmojis(mojis));
   }, []);
+  const showEmojiList = (event: React.SyntheticEvent) => {
+    let divTextContent = event.currentTarget as HTMLInputElement;
+    if (
+      divTextContent.textContent!.length === 1 &&
+      divTextContent.textContent!.charAt(0) === ":"
+    ) {
+      setIsEmojiComponentHidden(false);
+    } else {
+      setIsEmojiComponentHidden(true);
+    }
+  };
   const appendEmoji = (emojiValue: string) => {
     let img = document.createElement("img");
     img.className = "ml-1 w-5 h-5";
@@ -13,25 +27,33 @@ const EmojiComponent = ({ isEmojiComponentHidden }) => {
     inputFeild.current!.append(img);
   };
   return (
-    <div
-      ref={inputFeild}
-      className={`${
-        isEmojiComponentHidden ? "hidden" : ""
-      } border dark:border-gray-700 absolute top-[120%] z-10 flex-col dark:bg-[#161b22] rounded min-w-[230px]`}
-    >
-      {Object.keys(emojis)
-        ?.slice(0, 5)
-        .map((emojiKey, i) => (
-          <div
-            onClick={() => appendEmoji(emojis[emojiKey])}
-            key={`${emojiKey}__${i}`}
-            className="flex items-center border-b dark:border-gray-700 gap-2 px-3 py-2 dark:hover:bg-[#1f6feb] cursor-pointer"
-          >
-            <img className="w-5 h-5" src={emojis[emojiKey]} alt="emoji" />
-            <span className="font-bold text-lg text-white">{emojiKey}</span>
-          </div>
-        ))}
-    </div>
+    <>
+      <div
+        onInput={(e) => showEmojiList(e)}
+        contentEditable={true}
+        className=" h-10 flex items-center block w-full p-1.5 dark:text-gray-500 font-semibold border border-gray-300 rounded bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="⭐️ Type : to add emojis"
+        ref={inputFeild}
+      ></div>
+      <div
+        className={`${
+          isEmojiComponentHidden ? "hidden" : ""
+        } border dark:border-gray-700 absolute top-[120%] z-10 flex-col dark:bg-[#161b22] rounded min-w-[230px]`}
+      >
+        {Object.keys(emojis)
+          ?.slice(0, 5)
+          .map((emojiKey, i) => (
+            <div
+              onClick={() => appendEmoji(emojis[emojiKey])}
+              key={`${emojiKey}__${i}`}
+              className="flex items-center border-b dark:border-gray-700 gap-2 px-3 py-2 dark:hover:bg-[#1f6feb] cursor-pointer"
+            >
+              <img className="w-5 h-5" src={emojis[emojiKey]} alt="emoji" />
+              <span className="font-bold text-lg text-white">{emojiKey}</span>
+            </div>
+          ))}
+      </div>
+    </>
   );
 };
 
