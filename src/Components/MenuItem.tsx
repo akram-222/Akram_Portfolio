02a54b4c48b2e36359a1ecf997aca376f3d8e787
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-23 18:14:13 +0000

    Update MenuItem.tsx and projectItem.tsx

--- src/Components/MenuItem.tsx
+++ src/Components/MenuItem.tsx
@@ -3,7 +3,7 @@ import SidebarIcons from "./SidebarIcons";
 import { Link } from "react-router-dom";
 function MenuItem({ item: { id, title, notifications }, onClick, selected }) {
   return (
-    <div
+    <Link to={title.toLowerCase()}
       className={clsx(
         "w-full mt-6 flex items-center px-3 sm:px-0 xl:px-3 justify-start sm:justify-center xl:justify-start sm:mt-6 xl:mt-3 cursor-pointer",
         selected === id ? "sidebar-item-selected" : "sidebar-item"
@@ -11,16 +11,16 @@ function MenuItem({ item: { id, title, notifications }, onClick, selected }) {
       onClick={() => onClick(id)}
     >
       <SidebarIcons id={id} />
-      <Link to={title.toLowerCase()} className="block sm:hidden xl:block ml-2">
+      <span className="block sm:hidden xl:block ml-2">
         {title}
-      </Link>
+      </span>
       <div className="block sm:hidden xl:block flex-grow" />
       {notifications && (
         <div className="flex sm:hidden xl:flex dark:bg-pink-600  w-5 h-5 flex items-center justify-center rounded-full mr-2">
           <div className="text-white text-sm">{notifications}</div>
         </div>
       )}
-    </div>
+    </Link>
   );
 }
 
