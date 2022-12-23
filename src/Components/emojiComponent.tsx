c3b5ac4312a258d7a94b95b2f6e03e9369ca3b1e
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-23 12:13:20 +0000

    Update emojiComponent.tsx and __getEmoji.tsx

--- src/Components/emojiComponent.tsx
+++ src/Components/emojiComponent.tsx
@@ -1,12 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { __getEmojiList } from "../Utils/github/__gitEmojiList";
 const EmojiComponent = ({ isEmojiComponentHidden }) => {
   const [emojis, setEmojis] = useState({});
+  const inputFeild = useRef<HTMLDivElement | null>(null);
   useEffect(() => {
     __getEmojiList().then((mojis) => setEmojis(mojis));
   }, []);
+  const appendEmoji = (emojiValue: string) => {
+    let img = document.createElement("img");
+    img.className = "ml-1 w-5 h-5";
+    img.src = `${emojiValue}`;
+    inputFeild.current!.append(img);
+  };
   return (
     <div
+      ref={inputFeild}
       className={`${
         isEmojiComponentHidden ? "hidden" : ""
       } border dark:border-gray-700 absolute top-[120%] z-10 flex-col dark:bg-[#161b22] rounded min-w-[230px]`}
@@ -15,6 +23,7 @@ const EmojiComponent = ({ isEmojiComponentHidden }) => {
         ?.slice(0, 5)
         .map((emojiKey, i) => (
           <div
+            onClick={() => appendEmoji(emojis[emojiKey])}
             key={`${emojiKey}__${i}`}
             className="flex items-center border-b dark:border-gray-700 gap-2 px-3 py-2 dark:hover:bg-[#1f6feb] cursor-pointer"
           >
