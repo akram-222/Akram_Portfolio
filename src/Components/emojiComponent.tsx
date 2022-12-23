9eb4d3d444d73bc8ccda73503ab7679a2f2867f1
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-23 11:10:52 +0000

    Update Filter.tsx and emojiComponent.tsx

--- src/Components/emojiComponent.tsx
+++ src/Components/emojiComponent.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 import { __getEmojiList } from "../Utils/github/__gitEmojiList";
-const EmojiComponent = () => {
+const EmojiComponent = ({isEmojiComponentHidden}) => {
   const [emojis, setEmojis] = useState({});
   useEffect(() => {
     __getEmojiList().then((mojis) => setEmojis(mojis));
   }, []);
   return (
-    <div className="flex flex-wrap w-[300px] h-[300px] overflow-auto">
+    <div className={`${isEmojiComponentHidden ? "hidden":""} flex flex-wrap w-[300px] h-[300px] overflow-auto`}>
       {Object.keys(emojis)?.map((emojiKey) => (
         <img className="w-4 h-4" src={emojis[emojiKey]} alt="emoji" />
       ))}
