115559359d2da50c9e5fe77a7e5bf3a38b765d0e
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-24 14:12:48 +0000

    Update 3 files

--- src/Components/projectItem.tsx
+++ src/Components/projectItem.tsx
@@ -13,7 +13,7 @@ const ProjectItem = ({
     i + 1 + repoConfig.per_page * (repoConfig.page - 1) || i + 1;
   return (
     <div
-      className={`h-[130.43px] border relative w-full xs:w-[49%] md:w-[32%] bg-white rounded shadow-md px-4 dark:bg-[#171717] dark:border-[#353535] py-3`}
+      className={`randomGrayVariations-pattern h-[130.43px] border relative w-full xs:w-[49%] md:w-[32%] bg-white rounded shadow-md px-4 dark:bg-[#171717] dark:border-[#353535] py-3`}
     >
       <div className="">
         <div className="flex justify-between items-center">
@@ -22,7 +22,10 @@ const ProjectItem = ({
           </span>
 
           {/* {visibility} */}
-          <button className="cursor-help showHint" data-hint={visibility + ' repo'}>
+          <button
+            className="cursor-help showHint"
+            data-hint={visibility + " repo"}
+          >
             <Badge visibility={visibility} />
           </button>
         </div>
