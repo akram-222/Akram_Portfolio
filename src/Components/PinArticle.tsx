9ecb05accb6ec18225c33106b01e2a4b6c229a21
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-24 05:15:01 +0000

    Update PinArticle.tsx

--- src/Components/PinArticle.tsx
+++ src/Components/PinArticle.tsx
@@ -1,6 +1,4 @@
-import Badge from "./Badge";
-
-const PinArticle = ({ className, title, coverImage, createdAt, user }) => {
+const PinArticle = ({ className, title, createdAt, user }) => {
   return (
     <a
       className={`${className} inspiration__geometry--pattern h-60 mb-4 md:mb-0 w-full relative inline-block`}
@@ -8,16 +6,9 @@ const PinArticle = ({ className, title, coverImage, createdAt, user }) => {
     >
       <div
         className="bg-gradient-to-tr from-[#101010] absolute left-0 bottom-0 w-full h-full z-10"
-        style={{
-        }}
+        style={{}}
       ></div>
-      {/* <img
-        alt=""
-        src={coverImage}
-        className="absolute left-0 top-0  h-full z-0 object-cover"
-      /> */}
       <div className="p-4 absolute bottom-0 left-0 w-full z-20">
-       
         <h2 className=" xs:text-[3.5vw] md:text-[2.6vw] lg:text-[1.5vw] font-semibold text-gray-100 leading-tight">
           {title}
         </h2>
@@ -38,9 +29,9 @@ const PinArticle = ({ className, title, coverImage, createdAt, user }) => {
               })}{" "}
             </p>
           </div>
-           <span className="self-center justify-self-center px-4 py-1 font-bold text-sm dark:text-blue-400 dark:border-[#353535] border rounded ml-auto">
-          Tech
-        </span>
+          <span className="self-center justify-self-center px-4 py-1 font-bold text-sm dark:text-blue-400 dark:border-[#353535] border rounded ml-auto">
+            Tech
+          </span>
         </div>
       </div>
     </a>
