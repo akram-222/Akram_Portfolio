88415c36d3f594908669f0fa410377a7857f4698
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-24 16:21:50 +0000

    Update Notifications.tsx and index.css

--- src/Pages/Notifications.tsx
+++ src/Pages/Notifications.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { FaRegDotCircle } from "react-icons/fa";
 import PageTitle from "../Components/PageTitle";
 import { __getNotifications } from "../Utils/github/__getNotifications";
 
@@ -24,75 +25,84 @@ const Notifications = ({ onSidebarHide }) => {
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              <th scope="col" className="p-4">
+              <th scope="col" colSpan={7} className="p-4">
                 <div className="flex items-center">
                   <input
                     id="checkbox-all-search"
                     type="checkbox"
                     className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                   />
-                  <label htmlFor="checkbox-all-search" className="sr-only">
-                    checkbox
+                  <label
+                    htmlFor="checkbox-all-search"
+                    className="mx-2 text-sm font-bold dark:text-white"
+                  >
+                    Select All
                   </label>
                 </div>
-              </th>
-              <th scope="col" className="py-3 px-6">
-                Product name
-              </th>
-              <th scope="col" className="py-3 px-6">
-                Color
-              </th>
-              <th scope="col" className="py-3 px-6">
-                Category
-              </th>
-              <th scope="col" className="py-3 px-6">
-                Price
-              </th>
-              <th scope="col" className="py-3 px-6">
-                Action
               </th>
             </tr>
           </thead>
           <tbody>
-            {/* {notificationsList &&
-              notificationsList?.map((notify) => {
-                return (
-                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                    <td className="p-4 w-4">
-                      <div className="flex items-center">
-                        <input
-                          id="checkbox-table-search-3"
-                          type="checkbox"
-                          className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                        />
-                        <label
-                          htmlFor="checkbox-table-search-3"
-                          className="sr-only"
-                        >
-                          checkbox
-                        </label>
-                      </div>
-                    </td>
-                    <th
-                      scope="row"
-                      className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            {notificationsList &&
+              notificationsList?.map(
+                (
+                  {
+                    reason,
+                    repository: {
+                      name,
+                      id,
+                      owner: { login },
+                    },
+                    subject: { title },
+                    updated_at,
+                  },
+                  i
+                ) => {
+                  return (
+                    <tr
+                      key={i}
+                      className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
                     >
-                      AirTag
-                    </th>
-                    <td className="py-4 px-6">Silver</td>
-                    <td className="py-4 px-6">Accessories</td>
-                    <td className="py-4 px-6">$29</td>
-                    <td className="py-4 px-6">
-                      <a
-                        href="#"
-                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                      <td className="p-4 w-4">
+                        <div className="flex items-center">
+                          <input
+                            id="checkbox-table-search-3"
+                            type="checkbox"
+                            className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                          />
+                          <label
+                            htmlFor="checkbox-table-search-3"
+                            className="sr-only"
+                          >
+                            checkbox
+                          </label>
+                        </div>
+                      </td>
+                      <td className="flex items-center justify-center h-14">
+                        <FaRegDotCircle size={25} />
+                      </td>
+                      <th
+                        scope="row"
+                        className=" py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
                       >
-                        Edit
-                      </a>
-                    </td>
-                  </tr>
-                );
-              })} */}
+                        <div className="top_part">
+                          {login}/{name}
+                          <span className="mx-2 dark:text-gray-400">#{id}</span>
+                        </div>
+                        <div className="bottom_part expanded">[{title}]</div>
+                      </th>
+                      <td className="py-4 px-6">+10</td>
+                      <td className="py-4 px-6">{reason}</td>
+                      <td className="py-4 px-6">
+                        {new Date().getHours() -
+                          12 -
+                          (new Date(updated_at).getHours() - 12)}{" "}
+                        hours ago
+                      </td>
+                    </tr>
+                  );
+                }
+              )}
           </tbody>
         </table>
         <nav
@@ -124,9 +134,9 @@ const Notifications = ({ onSidebarHide }) => {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
               </a>
@@ -186,9 +196,9 @@ const Notifications = ({ onSidebarHide }) => {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
               </a>
