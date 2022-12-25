3c4528e04d33eb6fc53802daaf8beb9df7a9aded
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-25 10:40:49 +0000

    Update Login.tsx and OctokitConstructor.tsx

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,4 +1,26 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 const Login = () => {
+  const navigate = useNavigate();
+  const [accessTokenVal, setAccessTokenVal] = useState("");
+  const [colorState, setColorState] = useState("");
+  const handleInputValue = (e)=>{
+    setAccessTokenVal(e.currentTarget.value);
+    if (accessTokenVal === process.env.REACT_APP_GITHUB_ACCESS_TOKEN) {
+    setColorState("green");
+    }else{
+      setColorState("red");
+    }
+  }
+  const handleSigningInProcess = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    if (accessTokenVal === process.env.REACT_APP_GITHUB_ACCESS_TOKEN) {
+      navigate("/dashboard");
+      console.log('navigate')
+    } else {
+      console.log("disconnecting");
+    }
+  };
   return (
     <>
       <section className="bg-gray-50 dark:bg-gray-900 w-full">
@@ -20,22 +42,6 @@ const Login = () => {
                 Sign in to your account
               </h1>
               <form className="space-y-4 md:space-y-6" action="#">
-                {/* <div>
-                  <label
-                    htmlFor="email"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  >
-                    Your email
-                  </label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="name@company.com"
-                    required
-                  />
-                </div> */}
                 <div>
                   <label
                     htmlFor="password"
@@ -47,8 +53,10 @@ const Login = () => {
                     type="password"
                     name="password"
                     id="password"
-                    placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    value={accessTokenVal}
+                    onInput={(e) => handleInputValue(e)}
+                    placeholder="gh**************************************"
+                    className={`border border-gray-300 sm:text-sm text-${colorState}-400 rounded-lg focus:ring-${colorState}-600 focus:border-${colorState}-600 block w-full p-2.5 dark:bg-gray-700 dark:border-${colorState}-600 dark:placeholder-gray-400 dark:text-${colorState}-600 dark:focus:ring-${colorState}-500 dark:focus:border-${colorState}-500`}
                     required
                   />
                 </div>
@@ -59,7 +67,7 @@ const Login = () => {
                         id="remember"
                         aria-describedby="remember"
                         type="checkbox"
-                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
                         disabled
                       />
                     </div>
@@ -81,7 +89,8 @@ const Login = () => {
                 </div>
                 <button
                   type="submit"
-                  className="w-full text-white dark:bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                  className={`w-full text-white dark:bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800`}
+                  onClick={(e) => handleSigningInProcess(e)}
                 >
                   Sign in
                 </button>
