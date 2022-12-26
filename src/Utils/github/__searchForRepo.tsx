6e51fcaae0c874556e0d052a173346e93eb0fb0a
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-26 19:34:56 +0000

    Update emojiComponent.tsx and __searchForRepo.tsx

--- src/Utils/github/__searchForRepo.tsx
+++ src/Utils/github/__searchForRepo.tsx
@@ -1,10 +1,11 @@
 import { octokit } from "./OctokitConstructor";
 
-export async function __getRepo(repoName) {
-  return await octokit.request('GET /repos/{owner}/{repo}', {
-  owner: 'OWNER',
-  repo: repoName
-})
-    .then(({ data }) => data);
-
+export async function __getRepo(repoName: string) {
+  return await octokit
+    .request("GET /repos/{owner}/{repo}", {
+      owner: "ak-ram",
+      repo: repoName,
+    })
+    .then(({ data }) => data)
+    .catch(() => console.log("repo doesn't exist"));
 }
