ca1a671d9c2d7d834af2c1d5961d169ee959f0ba
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-26 12:41:44 +0000

    Update 4 files

--- src/Components/Pagination.tsx
+++ src/Components/Pagination.tsx
@@ -1,3 +1,5 @@
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+
 const Pagination = ({
   className,
   isLoad,
@@ -7,59 +9,101 @@ const Pagination = ({
   repos,
 }) => {
   return (
-    <nav aria-label="Page navigation" className={`${className} mt-8 flex`}>
-      <ul className="inline-flex -space-x-px w-fit mx-auto ">
-        <li>
-          <a
-            href="#d"
-            className="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-[#171717] dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+    <div className="flex w-full mt-5 space-x-2 justify-end">
+      <button
+        onClick={() => {
+          setLoad(true);
+          setRepoConfig({
+            ...repoConfig,
+            page: repoConfig.page === 1 ? 1 : repoConfig.page - 1,
+          });
+        }}
+        className="inline-flex items-center h-8 w-8 justify-center text-gray-400 rounded-md shadow border border-gray-200 dark:border-gray-800 leading-none"
+      >
+        <FaChevronLeft size={12} />
+      </button>
+      {[1, 2, 3, 4].map((item) => {
+        return (
+          <button
             onClick={() => {
               setLoad(true);
               setRepoConfig({
                 ...repoConfig,
-                page: repoConfig.page === 1 ? 1 : repoConfig.page - 1,
+                page: item,
               });
             }}
+            className="inline-flex items-center h-8 w-8 justify-center text-gray-500 rounded-md shadow border border-gray-200 dark:border-gray-800 leading-none"
           >
-            Previous
-          </a>
-        </li>
+            {item}
+          </button>
+        );
+      })}
+      <button
+        onClick={() => {
+          setLoad(true);
+          setRepoConfig({
+            ...repoConfig,
+            page: repoConfig.page === 4 ? 4 : repoConfig.page + 1,
+          });
+        }}
+        className="inline-flex items-center h-8 w-8 justify-center text-gray-400 rounded-md shadow border border-gray-200 dark:border-gray-800 leading-none"
+      >
+        <FaChevronRight size={12} />
+      </button>
+    </div>
+    // <nav aria-label="Page navigation" className={`${className} mt-8 flex`}>
+    //   <ul className="inline-flex -space-x-px w-fit mx-auto ">
+    //     <li>
+    //       <a
+    //         href="#d"
+    //         className="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-[#171717] dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+    //         onClick={() => {
+    //           setLoad(true);
+    //           setRepoConfig({
+    //             ...repoConfig,
+    //             page: repoConfig.page === 1 ? 1 : repoConfig.page - 1,
+    //           });
+    //         }}
+    //       >
+    //         Previous
+    //       </a>
+    //     </li>
 
-        {[1, 2, 3, 4].map((item, i) => (
-          <li key={i}>
-            <a
-              href="#d"
-              className="dark:focus:bg-gray-700 dark:focus:text-white px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-[#171717] dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              onClick={() => {
-                setLoad(true);
-                setRepoConfig({
-                  ...repoConfig,
-                  page: item,
-                });
-              }}
-            >
-              {item}
-            </a>
-          </li>
-        ))}
+    //     {[1, 2, 3, 4].map((item, i) => (
+    //       <li key={i}>
+    //         <a
+    //           href="#d"
+    //           className="inline-flex items-center h-8 w-8 justify-center text-gray-500 rounded-md shadow border border-gray-200 dark:border-gray-800 leading-none"
+    //           onClick={() => {
+    //             setLoad(true);
+    //             setRepoConfig({
+    //               ...repoConfig,
+    //               page: item,
+    //             });
+    //           }}
+    //         >
+    //           {item}
+    //         </a>
+    //       </li>
+    //     ))}
 
-        <li>
-          <a
-            href="#d"
-            className=" px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-[#171717] dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-            onClick={() => {
-              setLoad(true);
-              setRepoConfig({
-                ...repoConfig,
-                page: repoConfig.page === 4 ? 4 : repoConfig.page + 1,
-              });
-            }}
-          >
-            Next
-          </a>
-        </li>
-      </ul>
-    </nav>
+    //     <li>
+    //       <a
+    //         href="#d"
+    //         className=" px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-[#171717] dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+    //         onClick={() => {
+    //           setLoad(true);
+    //           setRepoConfig({
+    //             ...repoConfig,
+    //             page: repoConfig.page === 4 ? 4 : repoConfig.page + 1,
+    //           });
+    //         }}
+    //       >
+    //         Next
+    //       </a>
+    //     </li>
+    //   </ul>
+    // </nav>
   );
 };
 
--- src/Components/projectItem.tsx
+++ src/Components/projectItem.tsx
@@ -1,5 +1,6 @@
-import { BsCalendarDate, BsCodeSlash } from "react-icons/bs";
-import Badge from "./Badge";
+import { AiOutlineInfoCircle } from "react-icons/ai";
+import { IoIosMore } from "react-icons/io";
+
 const ProjectItem = ({
   name,
   createdAt,
@@ -12,47 +13,66 @@ const ProjectItem = ({
   const indexingSys =
     i + 1 + repoConfig.per_page * (repoConfig.page - 1) || i + 1;
   return (
-    <div
-      className={`randomGrayVariations-pattern h-[130.43px] border relative w-full xs:w-[49%] md:w-[32%] bg-white rounded-xl shadow-md px-4 dark:bg-[#171717] dark:border-[#353535] py-3`}
-    >
-      <div className="">
-        <div className="flex justify-between items-center">
-          <span className="text-white font-semibold my-2 whitespace-nowrap overflow-hidden w-2/3 text-ellipsis">
-            {indexingSys}. {name.replace("-", " ")}
-          </span>
-
-          {/* {visibility} */}
-          <button
-            className="cursor-help showHint flex"
-            data-hint={visibility + " repo"}
+    <tr>
+      <td className="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800">
+        <div className="flex items-center">
+          <AiOutlineInfoCircle size={20} className="mr-4 text-yellow-500" />
+          {indexingSys}. {name.replace("-", " ")}
+        </div>
+      </td>
+      <td className="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800">
+        <div className="flex items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512.001 512.001"
+            className="w-7 h-7 p-1.5 mr-2.5 rounded-lg border border-gray-200 dark:border-gray-800"
           >
-            <Badge visibility={visibility} />
-          </button>
+            <path
+              fill="#03a9f4"
+              d="M425.457 117.739c-3.121-1.838-6.961-1.966-10.197-.341-3.231 1.629-5.416 4.786-5.803 8.384-.384 3.499-.981 6.997-1.728 10.667-20.885 94.784-62.827 140.885-128.256 140.885h-96c-5.062.009-9.42 3.574-10.432 8.533l-32 149.995-5.717 38.187c-3.287 17.365 8.125 34.107 25.489 37.394 1.915.362 3.858.549 5.807.558h64.213c14.718.045 27.55-10 31.04-24.299l25.941-103.701h55.659c65.685 0 111.083-52.373 127.829-147.477 11.054-45.286-7.234-92.668-45.845-118.785z"
+            ></path>
+            <path
+              fill="#283593"
+              d="M405.339 38.017C384.261 14.108 354.012.286 322.139.001h-176.64C119.064-.141 96.558 19.2 92.721 45.355L37.873 411.243c-2.627 17.477 9.41 33.774 26.887 36.402 1.586.239 3.189.357 4.793.356h81.92c5.062-.009 9.42-3.574 10.432-8.533l30.187-140.8h87.467c75.904 0 126.059-53.056 149.099-157.867.926-4.178 1.638-8.4 2.133-12.651 5.348-32.335-3.981-65.372-25.452-90.133z"
+            ></path>
+          </svg>
+
+          {fork
+            ? "Fork"
+            : language
+            ? language
+            : name.toLowerCase() === "Ak-ram".toLocaleLowerCase()
+            ? "README Profile"
+            : "Empty Repo"}
         </div>
-        <div className="flex space-x-2 text-gray-400 text-sm">
-          <BsCodeSlash size={20} />
-          <p>
-            {fork
-              ? "Fork"
-              : language
-              ? language
-              : name.toLowerCase() === "Ak-ram".toLocaleLowerCase()
-              ? "README Profile"
-              : "Empty Repo"}
-          </p>
-        </div>
-        <div className="flex space-x-2 text-gray-400 text-sm my-3">
-          <BsCalendarDate size={18} />
-          <p>
+      </td>
+      <td className="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800 md:table-cell hidden">
+        Subscription renewal
+      </td>
+      <td className="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800 text-red-500">
+        {visibility}
+      </td>
+      <td className="sm:p-3 py-2 px-1 border-b border-gray-200 dark:border-gray-800">
+        <div className="flex items-center">
+          <div className="sm:flex hidden flex-col">
             {new Date(createdAt).toLocaleDateString("en-US", {
               day: "numeric",
               month: "short",
               year: "numeric",
             })}
-          </p>
+            <div className="text-gray-400 text-xs">
+              {new Date(createdAt).toLocaleTimeString("en-US", {
+                hour: "2-digit",
+                minute: "2-digit",
+              })}
+            </div>
+          </div>
+          <button className="w-8 h-8 inline-flex items-center justify-center text-gray-400 ml-auto">
+            <IoIosMore />
+          </button>
         </div>
-      </div>
-    </div>
+      </td>
+    </tr>
   );
 };
 
