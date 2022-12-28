1bd424af3ef21c1f44d2da7d80258f955348db73
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-28 17:22:18 +0000

    Update SkillsSlider.tsx

--- src/Components/SkillsSlider.tsx
+++ src/Components/SkillsSlider.tsx
@@ -460,28 +460,28 @@ const Skills = () => {
             <div className="grid grid-cols-6 col-span-2   gap-2  ">
               <div className=" overflow-hidden col-span-3 max-h-[14rem]">
                 <img
-                  className="h-full w-full object-cover "
+                  className="brightness-50 h-full w-full object-cover "
                   src="https://images.unsplash.com/photo-1517487881594-2787fef5ebf7?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=735&amp;q=80"
                   alt=""
                 />
               </div>
               <div className=" overflow-hidden col-span-3 max-h-[14rem]">
                 <img
-                  className="h-full w-full object-cover  "
+                  className="brightness-50 h-full w-full object-cover  "
                   src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1399&amp;q=80"
                   alt=""
                 />
               </div>
               <div className=" overflow-hidden  col-span-2 max-h-[10rem]">
                 <img
-                  className="h-full w-full object-cover "
+                  className="brightness-50 h-full w-full object-cover "
                   src="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1470&amp;q=80"
                   alt=""
                 />
               </div>
               <div className=" overflow-hidden  col-span-2 max-h-[10rem]">
                 <img
-                  className="h-full w-full object-cover "
+                  className="brightness-50 h-full w-full object-cover "
                   src="https://images.unsplash.com/photo-1503602642458-232111445657?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80"
                   alt=""
                 />
@@ -491,7 +491,7 @@ const Skills = () => {
                   + 23
                 </div>
                 <img
-                  className="h-full w-full object-cover "
+                  className="brightness-50 h-full w-full object-cover "
                   src="https://images.unsplash.com/photo-1560393464-5c69a73c5770?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=765&amp;q=80"
                   alt=""
                 />
