16f51b2ecd491374c38c54d2c3e45f7a04da0075
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-28 17:18:40 +0000

    Update SkillsSlider.tsx

--- src/Components/SkillsSlider.tsx
+++ src/Components/SkillsSlider.tsx
@@ -458,35 +458,35 @@ const Skills = () => {
           <div className="border-b border-b-[#353535]"></div>
           <div className="text-gray-400 font-medium text-sm mb-7 mt-6 mx-3 px-2">
             <div className="grid grid-cols-6 col-span-2   gap-2  ">
-              <div className=" overflow-hidden rounded-xl col-span-3 max-h-[14rem]">
+              <div className=" overflow-hidden col-span-3 max-h-[14rem]">
                 <img
                   className="h-full w-full object-cover "
                   src="https://images.unsplash.com/photo-1517487881594-2787fef5ebf7?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=735&amp;q=80"
                   alt=""
                 />
               </div>
-              <div className=" overflow-hidden rounded-xl col-span-3 max-h-[14rem]">
+              <div className=" overflow-hidden col-span-3 max-h-[14rem]">
                 <img
                   className="h-full w-full object-cover  "
                   src="https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1399&amp;q=80"
                   alt=""
                 />
               </div>
-              <div className=" overflow-hidden rounded-xl col-span-2 max-h-[10rem]">
+              <div className=" overflow-hidden  col-span-2 max-h-[10rem]">
                 <img
                   className="h-full w-full object-cover "
                   src="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=1470&amp;q=80"
                   alt=""
                 />
               </div>
-              <div className=" overflow-hidden rounded-xl col-span-2 max-h-[10rem]">
+              <div className=" overflow-hidden  col-span-2 max-h-[10rem]">
                 <img
                   className="h-full w-full object-cover "
                   src="https://images.unsplash.com/photo-1503602642458-232111445657?ixlib=rb-1.2.1&amp;ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&amp;auto=format&amp;fit=crop&amp;w=687&amp;q=80"
                   alt=""
                 />
               </div>
-              <div className="relative overflow-hidden rounded-xl col-span-2 max-h-[10rem]">
+              <div className="relative overflow-hidden  col-span-2 max-h-[10rem]">
                 <div className="text-white text-xl absolute inset-0  bg-slate-900/80 flex justify-center items-center">
                   + 23
                 </div>
