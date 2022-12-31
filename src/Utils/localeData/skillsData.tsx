7506a9a2a1704df975036b78963545a819ca08e2
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-31 12:55:47 +0000

    Update tabContent.tsx and skillsData.tsx

--- src/Utils/localeData/skillsData.tsx
+++ src/Utils/localeData/skillsData.tsx
@@ -1,4 +1,5 @@
 import { CgAppleWatch, CgScreen, CgServer } from "react-icons/cg";
+import { TbBrandJavascript } from "react-icons/tb";
 import TabContent from "../../Components/tabContent";
 
 export const skillsTab: { tabName: string; tabIcon: React.ReactNode }[] = [
@@ -6,8 +7,14 @@ export const skillsTab: { tabName: string; tabIcon: React.ReactNode }[] = [
   { tabName: "Backend", tabIcon: <CgServer size={20} /> },
   { tabName: "Softwares", tabIcon: <CgAppleWatch size={20} /> },
 ];
+
+export const frontendSkillsList: {
+  skillIcon: React.ReactNode;
+  skillName: string;
+}[] = [{ skillIcon: <TbBrandJavascript />, skillName: "Javascript" }];
+
 export const skillsTabContent = {
-  0: <TabContent />,
+  0: <TabContent list={frontendSkillsList} />,
   1: "tab2",
   2: "tab3",
 };
