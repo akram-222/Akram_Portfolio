094cfd57117b75e9e69695e301f83d887193c551
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-31 18:27:12 +0000

    Update skillsData.tsx

--- src/Utils/localeData/skillsData.tsx
+++ src/Utils/localeData/skillsData.tsx
@@ -1,5 +1,19 @@
 import { CgAppleWatch, CgScreen, CgServer } from "react-icons/cg";
-import { TbBrandJavascript } from "react-icons/tb";
+import {
+  SiBootstrap,
+  SiCss3,
+  SiFirebase,
+  SiGit,
+  SiGithub,
+  SiHtml5,
+  SiJavascript,
+  SiPostman,
+  SiReact,
+  SiReactrouter,
+  SiSass,
+  SiTailwindcss,
+  SiTypescript,
+} from "react-icons/si";
 import TabContent from "../../Components/tabContent";
 
 export const skillsTab: { tabName: string; tabIcon: React.ReactNode }[] = [
@@ -11,10 +25,34 @@ export const skillsTab: { tabName: string; tabIcon: React.ReactNode }[] = [
 export const frontendSkillsList: {
   skillIcon: React.ReactNode;
   skillName: string;
-}[] = [{ skillIcon: <TbBrandJavascript />, skillName: "Javascript" }];
+}[] = [
+  { skillIcon: <SiReact />, skillName: "React" },
+  { skillIcon: <SiTypescript />, skillName: "Typescript" },
+  { skillIcon: <SiJavascript />, skillName: "Javascript" },
+  { skillIcon: <SiReactrouter />, skillName: "React-Router" },
+  { skillIcon: <SiSass />, skillName: "Sass" },
+  { skillIcon: <SiCss3 />, skillName: "Css3" },
+  { skillIcon: <SiTailwindcss />, skillName: "Tailwind" },
+  { skillIcon: <SiBootstrap />, skillName: "Bootstrap" },
+  { skillIcon: <SiHtml5 />, skillName: "HTML5" },
+];
+
+export const backendSkillsList: {
+  skillIcon: React.ReactNode;
+  skillName: string;
+}[] = [{ skillIcon: <SiFirebase />, skillName: "Firebase" }];
+
+export const softwareSkillsList: {
+  skillIcon: React.ReactNode;
+  skillName: string;
+}[] = [
+  { skillIcon: <SiPostman />, skillName: "Postman" },
+  { skillIcon: <SiGit />, skillName: "Git" },
+  { skillIcon: <SiGithub />, skillName: "Github" },
+];
 
 export const skillsTabContent = {
   0: <TabContent list={frontendSkillsList} />,
-  1: "tab2",
-  2: "tab3",
+  1: <TabContent list={backendSkillsList} />,
+  2: <TabContent list={softwareSkillsList} />,
 };
