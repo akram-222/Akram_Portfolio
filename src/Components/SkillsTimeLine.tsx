5e52213e067569bb29035f5bd794e10fb3829175
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2022-12-31 09:54:31 +0000

    Update 3 files

--- src/Components/SkillsTimeLine.tsx
+++ src/Components/SkillsTimeLine.tsx
@@ -1,355 +1,42 @@
-// .seccion
-// {
-//     text-transform: uppercase;
-//     -webkit-transform: rotate(270deg);
-//     -moz-transform: rotate(270deg);
-//     -ms-transform: rotate(270deg);
-//     -o-transform: rotate(270deg);
-//     transform: rotate(270deg);
-//     -webkit-transform-origin: 0 0;
-//     -moz-transform-origin: 0 0;
-//     -ms-transform-origin: 0 0;
-//     -o-transform-origin: 0 0;
-//     transform-origin: 0 0;
-//     top: 60%;
-// }
+import { CgAppleWatch, CgScreen, CgServer } from "react-icons/cg";
 const SkillsTimeLine = () => {
+  const tabs: { tabName: string; tabIcon: React.ReactNode }[] = [
+    { tabName: "Frontend", tabIcon: <CgScreen size={20} /> },
+    { tabName: "Backend", tabIcon: <CgServer size={20} /> },
+    { tabName: "Softwares", tabIcon: <CgAppleWatch size={20} /> },
+  ];
+  const switcher = (
+    e: React.SyntheticEvent,
+    classValue: string,
+    siblingsSelector: string
+  ) => {
+    let targetedElement = e.currentTarget as HTMLElement;
+    let siblings =
+      targetedElement.parentElement!.querySelectorAll(siblingsSelector);
+    [...siblings]?.map((sibling) => sibling.classList.remove(classValue));
+    targetedElement.classList.add(classValue);
+  };
   return (
-    <div className="p-4 md:p-10 bg-white dark:bg-transparent">
-      <div className="md:flex mt-20 text-center ml-10 mr-10 md:ml-36 md:mr-36">
-        <div className="md:mr-20 md:w-1/3">
-          <h1 className="font-bold italic mb-10">TECNOLOGIAS QUE MÁS USO</h1>
-          <div className="relative mt-5 text-left">
-            <div className="flex items-center relative pb-5 justify-start">
-              <div className="border-r-2 border-black absolute h-full top-2 z-10 ">
-                <i className="fas fa-circle -top-1 -ml-2 absolute"></i>
-                <div className="absolute seccion -ml-8 font-bold italic text-gray-400 ">
-                  BACKEND
-                </div>
-              </div>
-
-              <div className="ml-6 pt-5">
-                <div className="flex mb-4 space-x-16">
-                  <div>1</div>
-                  <div>2 </div>
-                  <div>
-                    <img
-                      className="w-24"
-                      src="https://img.icons8.com/ios/452/python.png"
-                    />
-                  </div>
-                </div>
-                <div className="flex mb-4 space-x-16">
-                  <div>3 </div>
-                  <div>
-                    <img
-                      className="w-24"
-                      src="https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/digitalocean-512.png"
-                    />
-                  </div>
-                  <div>
-                    <img
-                      className="w-24"
-                      src="https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/postgresql-512.png"
-                    />
-                  </div>
-                </div>
-                <div className="flex mb-4 space-x-16">
-                  <div>
-                    <img
-                      className="w-24"
-                      src="https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/mysql-512.png"
-                      alt=""
-                    />
-                  </div>
-                  <div>
-                    <img
-                      className="w-24"
-                      src="https://mariadb.com/wp-content/uploads/2019/11/mariadb-logo-vert_black-transparent.png"
-                    />
-                  </div>
-                  <div>
-                    <img
-                      className="w-24"
-                      src="https://cdns.iconmonstr.com/wp-content/assets/preview/2013/240/iconmonstr-linux-os-1.png"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="flex items-center relative pb-5 pt-5 justify-start">
-              <div className="border-r-2 border-black absolute h-full top-2 z-10 ">
-                <i className="fas fa-circle -top-1 -ml-2 absolute"></i>
-                <div className="absolute seccion -ml-8 font-bold italic text-gray-400">
-                  SOFTWARE
-                </div>
-              </div>
-
-              <div className="ml-6 pt-5">
-                <div className="flex mb-4 space-x-16">
-                  <div>4 </div>
-                  <div>
-                    <img
-                      className="w-24"
-                      style={{ filter: "contrast(0.01%) brightness(10%)" }}
-                      src="https://i2.wp.com/mac-torrent-download.net/wp-content/uploads/2019/11/Navicat-Premium-15-icon.png?resize=246%2C246&ssl=1"
-                    />
-                  </div>
-                  <div>
-                    <img
-                      className="w-24"
-                      src="https://img.icons8.com/ios-filled/452/burp-suite.png"
-                    />
-                  </div>
-                </div>
-                <div className="flex mb-4 space-x-16">
-                  <div>
-                    <img
-                      className="w-24"
-                      src="https://i.imgur.com/VCRcgf2.png"
-                    />
-                  </div>
-                  <div>
-                    <img
-                      className="w-24"
-                      src="https://i.imgur.com/pXHzSuT.png"
-                    />
-                  </div>
-                  <div>
-                    <img
-                      className="w-24"
-                      src="https://i.imgur.com/Hen7iya.png"
-                    />
-                  </div>
-                </div>
-                <div className="flex mb-4 space-x-16">
-                  <div>
-                    <img
-                      className="w-24"
-                      src="https://img.icons8.com/ios/452/xcode.png"
-                    />
-                  </div>
-                  <div>
-                    <img
-                      className="w-24"
-                      style={{ filter: "contrast(0.01%) brightness(10%)" }}
-                      src="https://cdn.clipart.email/f7b6e038eb20af29a2864eebc3dd7a2b_android-studio-logo-png-picture-378911-android-studio-logo-png_512-512.png"
-                    />
-                  </div>
-                  <div>
-                    <img
-                      className="w-24"
-                      src="https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/postman-512.png"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="flex items-center relative pb-5 pt-5 justify-start">
-              <div className="border-r-2 border-black absolute h-full top-2 z-10 ">
-                <i className="fas fa-circle -top-1 -ml-2 absolute"></i>
-                <div className="absolute seccion -ml-8 font-bold italic text-gray-400 top-32">
-                  FRONTEND
-                </div>
-              </div>
-
-              <div className="ml-6 pt-5">
-                <div className="flex mb-4 space-x-16">
-                  <div>
-                    <img
-                      className="w-24"
-                      src="https://www.flaticon.com/svg/static/icons/svg/1216/1216909.svg"
-                      alt=""
-                    />
-                  </div>
-                  <div>
-                    <img
-                      className="w-24"
-                      src="https://cdn4.iconfinder.com/data/icons/scripting-and-programming-languages/512/JQuery_logo-512.png"
-                    />
-                  </div>
-                  <div>5 </div>
-                </div>
-                <div className="flex mb-4 space-x-16">
-                  <div>
-                    <img
-                      className="w-24"
-                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFpfYz4DA8fBtTByR0UPNsMX5I2FbWUrYOjw&usqp=CAU"
-                    />
-                  </div>
-                  <div>6 </div>
-                  <div>
-                    <img
-                      className="w-24"
-                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk5CrEfm5Q_4rOst-cvdiTGBeX8XQjuc2Juw&usqp=CAU"
-                    />
-                  </div>
-                </div>
-                <div className="flex mb-4 space-x-16">
-                  <div>7 </div>
-                  <div>
-                    <img
-                      className="w-24"
-                      style={{ filter: "contrast(90%) brightness(50%)" }}
-                      src="https://www.markusantonwolf.com/media/pages/blog/alpine-js/1468511062-1596675049/alpinejs-logo.svg"
-                    />
-                  </div>
-                  <div>
-                    <img
-                      className="w-24"
-                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWS2CD7XCCxERnQJHaAfvsE8HlYoIiORxKlw&usqp=CAU"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="flex items-center relative pb-5 pt-5 justify-start">
-              <div className="border-r-2 border-black absolute h-full top-2 z-10">
-                <i className="fas fa-circle -top-1 -ml-2 absolute"></i>
-                <div
-                  style={{ top: "80%" }}
-                  className="absolute seccion -ml-8 font-bold italic text-gray-400"
-                >
-                  DESAROLLO&nbsp;MÓVIL
-                </div>
-                <i className="fas fa-circle -bottom-3 -ml-2 absolute"></i>
-              </div>
-
-              <div className="ml-6 pt-5">
-                <div className="flex mb-4 space-x-16">
-                  <div>8 </div>
-                  <div>
-                    <img
-                      className="w-24"
-                      src="https://i.imgur.com/7AaIwV8.png"
-                    />
-                  </div>
-                  <div>9 </div>
-                </div>
-                <div className="flex mb-4 space-x-16">
-                  <div>
-                    <img
-                      className="w-24"
-                      src="https://img.icons8.com/ios/452/apple-app-store--v2.png"
-                    />
-                  </div>
-                  <div>
-                    <img
-                      className="w-24"
-                      src="https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/firebase-512.png"
-                    />
-                  </div>
-                  <div>
-                    <img
-                      className="w-24"
-                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvFuV7rL4NVvgHYU0IKbiNQx0dneHaguXh2w&usqp=CAU"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="md:w-2/3">
-          <h1 className="font-bold italic mb-10">PROYECTOS</h1>
-
-          <div className="relative mt-5 text-left">
-            <div className="flex items-center relative">
-              <div className="hidden md:block w-20">
-                <div className="font-bold italic">2020</div>
-                <div className="md:flex space-x-1 text-xs">
-                  <div>Abr.</div>
-                  <div>-</div>
-                  <div>Jun.</div>
-                </div>
-              </div>
-
-              <div className="border-r-2 border-black absolute h-full left-1 md:left-20 top-2 z-10">
-                <i className="fas fa-circle -top-1 -ml-2 absolute"></i>
-              </div>
-
-              <div className="ml-10">
-                <div className="font-bold">Proyecto 1</div>
-                <div className="italic md:mb-4">Empresa 1</div>
-                <div className="mb-4 mt-2 md:hidden">
-                  <div className="font-bold">2020</div>
-                  <div className="text-xs">Abril - Junio</div>
-                </div>
-                <div className="mb-10">
-                  Fusce auctor gravida dui, ut tristique nisi aliquam quis.
-                  Maecenas id ligula ac dui mollis tempor. Sed vitae ex eros.
-                  Proin nisl felis, consectetur sed elit sed, vestibulum
-                  ultrices nibh.
-                </div>
-              </div>
-            </div>
-
-            <div className="flex items-center relative">
-              <div className="hidden md:block w-20">
-                <div className="font-bold italic">2020</div>
-                <div className="md:flex space-x-1 text-xs">
-                  <div>Jul.</div>
-                  <div>-</div>
-                  <div>Ago.</div>
-                </div>
-              </div>
-
-              <div className="border-r-2 border-black absolute h-full left-1 md:left-20 top-2 z-10">
-                <i className="fas fa-circle -top-1 -ml-2 absolute"></i>
-              </div>
-
-              <div className="ml-10">
-                <div className="font-bold">Proyecto 2</div>
-                <div className="italic md:mb-4">Empresa 2</div>
-                <div className="mb-4 mt-2 md:hidden">
-                  <div className="font-bold">2020</div>
-                  <div className="text-xs">Julio - Agosto</div>
-                </div>
-                <div className="mb-10">
-                  Fusce auctor gravida dui, ut tristique nisi aliquam quis.
-                  Maecenas id ligula ac dui mollis tempor. Sed vitae ex eros.
-                  Proin nisl felis, consectetur sed elit sed, vestibulum
-                  ultrices nibh.
-                </div>
-              </div>
-            </div>
-
-            <div className="flex items-center relative">
-              <div className="hidden md:block w-20">
-                <div className="font-bold italic">2020</div>
-                <div className="md:flex space-x-1 text-xs">
-                  <div>Ago.</div>
-                  <div>-</div>
-                  <div>Hoy.</div>
-                </div>
-              </div>
-
-              <div className="border-r-2 border-black absolute h-full left-1 md:left-20 top-2 z-10">
-                <i className="fas fa-circle -top-1 -ml-2 absolute"></i>
-                <i className="fas fa-circle bottom-0 -ml-2 absolute"></i>
-              </div>
-
-              <div className="ml-10">
-                <div className="font-bold">Proyecto 3</div>
-                <div className="italic md:mb-4">Empresa 3</div>
-                <div className="mb-4 mt-2 md:hidden">
-                  <div className="font-bold">2020</div>
-                  <div className="text-xs">Agosto - Hoy</div>
-                </div>
-                <div className="mb-10">
-                  Fusce auctor gravida dui, ut tristique nisi aliquam quis.
-                  Maecenas id ligula ac dui mollis tempor. Sed vitae ex eros.
-                  Proin nisl felis, consectetur sed elit sed, vestibulum
-                  ultrices nibh.
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div className="bg-white dark:bg-[#171717]">
+      <div className="">
+        <ul className="flex justify-between flex-wrap text-sm font-medium text-center dark:border-b dark:border-[#353535]">
+          {tabs?.map(({ tabName, tabIcon }, index) => (
+            <li
+              className={`tabMember ${!index ? "active" : ""}`}
+              onClick={(e) => switcher(e, "active", ".tabMember")}
+            >
+              <a
+                href="#d"
+                className=" inline-flex gap-2 p-4 rounded-t-lg group"
+                aria-current="page"
+              >
+                {tabIcon}
+                {tabName}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="tabContent p-5">tab content</div>
       </div>
     </div>
   );
