18365bdadd908ced9e42cfb62c9ce3ff76f61ee8
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2023-01-02 17:44:53 +0000

    Update Steps.tsx and addNewEvent.tsx

--- src/Components/Steps.tsx
+++ src/Components/Steps.tsx
@@ -1,19 +1,24 @@
-const Steps = ({numberOfSteps,isCurrentStep})=>{
-return(
-    <div className="space-y-2 mb-3 dark:text-gray-100">
-	<h3 className="text-xs font-semibold">Step 3: Fill in order details</h3>
-	<div className="flex max-w-xs space-x-1">
-        {new Array(numberOfSteps).fill('').map((step,i)=>
-        
-		<span className={`w-8 h-1 ${isCurrentStep === i ? "dark:bg-gray-100":isCurrentStep>i?"dark:bg-violet-400":'dark:bg-gray-600'}`}></span>
-        )}
-		{/* <span className="w-8 h-1 dark:bg-violet-400"></span>
-		<span className="w-8 h-1 dark:bg-gray-100"></span>
-		<span className="w-8 h-1 dark:bg-gray-600"></span>
-		<span className="w-8 h-1 dark:bg-gray-600"></span> */}
-	</div>
-</div>
-)
-}
+const Steps = ({ numberOfSteps, isCurrentStep }) => {
+  return (
+    <div className={`space-y-2 mb-3 dark:text-gray-100`}>
+      <h3 className="text-xs font-semibold">Step 3: Fill in order details</h3>
+      <div className="flex max-w-xs space-x-1">
+        {new Array(numberOfSteps).fill("").map((step: string, i: number) => (
+          <span
+            className={`w-8 h-1 ${
+              +isCurrentStep === i + 1
+                ? "dark:bg-gray-100"
+                : isCurrentStep > i
+                ? "dark:bg-violet-400"
+                : isCurrentStep === "Done"
+                ? "dark:bg-violet-400"
+                : "dark:bg-gray-600"
+            }`}
+          ></span>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Steps
+export default Steps;
--- src/Components/addNewEvent.tsx
+++ src/Components/addNewEvent.tsx
@@ -1,12 +1,18 @@
 import Steps from "./Steps";
-import {useState} from "react";
-import {BsArrowRightShort} from "react-icons/bs";
+import { useState } from "react";
+import { BsArrowRightShort } from "react-icons/bs";
 const AddNewEvent = () => {
-    const [isCurrentStep,setCurrentStep] = useState(0)
+  const [isCurrentStep, setCurrentStep] = useState<number | "Done">(0);
+  const numberOfSteps: number = 3;
+  const handleCurrentStep = () => {
+    if (+isCurrentStep < numberOfSteps) setCurrentStep(+isCurrentStep + 1);
+    if (+isCurrentStep === numberOfSteps) setCurrentStep("Done");
+    if (isCurrentStep === "Done") return;
+  };
   return (
     <div className="absolute z-20 flex items-center justify-center absolute top-0 w-full h-full dark:bg-[#171717]">
       <form className="flex flex-col border w-1/2 p-4  dark:border-gray-600/30 rounded-lg">
-     <Steps numberOfSteps={3} isCurrentStep={isCurrentStep}/>
+        <Steps numberOfSteps={numberOfSteps} isCurrentStep={isCurrentStep} />
         <div className="flex border-t dark:border-gray-600/30 flex-col items-start mb-6">
           <div className="w-full mt-8">
             <label
@@ -23,15 +29,19 @@ const AddNewEvent = () => {
               required
             />
           </div>
-          
         </div>
         <button
           type="button"
-          onClick={()=> setCurrentStep(isCurrentStep + 1)}
+          onClick={() => handleCurrentStep()}
           className="ml-auto inline-flex items-center gap-2 text-blue-400 text-sm sm:w-auto px-3 py-1.5 text-center border dark:border-gray-600/30"
         >
-          Next <BsArrowRightShort size={20} />
-          
+          {isCurrentStep !== "Done" ? (
+            <>
+              Next <BsArrowRightShort size={20} />
+            </>
+          ) : (
+            "done"
+          )}
         </button>
       </form>
     </div>
