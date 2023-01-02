03c2dcd13cada71a2cc980912e570d6fd7ff4e3e
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2023-01-02 09:17:01 +0000

    Update Calendar.tsx and Settings.tsx

--- src/Components/Calendar.tsx
+++ src/Components/Calendar.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { BiChevronRight, BiChevronLeft } from "react-icons/bi";
 import { IoIosClose } from "react-icons/io";
-const Calendar = ({ isHidden, setIsHidden }) => {
+const Calendar = () => {
   const [isDateUpdate, setDateUpdate] = useState(false);
   const [sinceValue, setSinceValue] = useState(true);
   const [day, setDay] = useState(new Date().getDate());
@@ -47,24 +47,10 @@ const Calendar = ({ isHidden, setIsHidden }) => {
 
   return (
     <>
-      <div
-        className={`${
-          isHidden ? "hidden " : ""
-        } absolute overflow-hidden w-full h-full top-0 left-0 z-10 calendar-backdrop flex items-center justify-center py-8 px-4`}
-      >
-        <div className="tilt-in-right-1  xs:max-w-sm md:max-w-md shadow-lg">
-          <div className="md:p-8 p-5 dark:bg-gray-800 bg-white rounded-t">
-            <span className="flex justify-between items-center -my-3 -mx-2 mb-2 dark:bg-gray-700 px-2">
-              <span className="dark:text-green-400 font-bold">
-                Strive & let success chase you
-              </span>
-              <IoIosClose
-                size={35}
-                className="cursor-pointer dark:text-gray-500 dark:hover:text-rose-400 "
-                onClick={() => setIsHidden(!isHidden)}
-              />
-            </span>
-            <div className="flex items-center justify-between">
+      <div className={`w-full`}>
+        <div className="w-full">
+          <div className="p-5 dark:bg-[#171717] bg-white rounded-t">
+            <div className="dark:bg-gray-700/20 p-2 rounded flex items-center justify-between">
               <span
                 tabIndex={0}
                 className="focus:outline-none  text-base font-bold dark:text-gray-100 text-gray-800"
@@ -98,44 +84,47 @@ const Calendar = ({ isHidden, setIsHidden }) => {
               Bug: Upscaling elements of days on clicking, which lead to expanding the parent and appear it's scroll bar for milliseconds as a flasing.
               Solve: Add overflow-hidden class in line 106 instead of overflow-x-auto
              */}
-            <div className="flex gap-x-4 gap-y-2 px-3 items-center flex-wrap pt-6 overflow-hidden">
-              {weekdays.map((day, i) => {
-                return (
-                  <div
-                    key={`${day}__${i}`}
-                    className="w-8 h-8 flex items-center dark:text-gray-100 justify-center"
-                  >
-                    {day}
-                  </div>
-                );
-              })}
-
-              {["", "", "", ...monthDays].map((day, i) => {
-                return (
-                  <div
-                    key={`${day}__${i}`}
-                    onClick={() => setDay(+day)}
-                    onMouseDown={(e) => {
-                      setDateUpdate(true);
-                      e.currentTarget.classList.add("animate-ping-once");
-                    }}
-                    onMouseUp={(e) => {
-                      setTimeout(() => {
-                        setDateUpdate(false);
-                      }, 300);
-                      e.currentTarget.classList.remove("animate-ping-once");
-                    }}
-                    className={`${
-                      day.toString().length
-                        ? "dark:hover:bg-[#050708]/40 cursor-pointer dark:hover:text-blue-400"
-                        : ""
-                    } 
+            <div className="pt-6">
+              <div className="flex flex-nowrap">
+                {weekdays.map((day, i) => {
+                  return (
+                    <div
+                      key={`${day}__${i}`}
+                      className="w-[14vw] h-8 items-center dark:text-gray-100 justify-center"
+                    >
+                      {day}
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="flex">
+                {monthDays.map((day, i) => {
+                  return (
+                    <div
+                      key={`${day}__${i}`}
+                      onClick={() => setDay(+day)}
+                      onMouseDown={(e) => {
+                        setDateUpdate(true);
+                        e.currentTarget.classList.add("animate-ping-once");
+                      }}
+                      onMouseUp={(e) => {
+                        setTimeout(() => {
+                          setDateUpdate(false);
+                        }, 300);
+                        e.currentTarget.classList.remove("animate-ping-once");
+                      }}
+                      className={`${
+                        day.toString().length
+                          ? "dark:hover:bg-[#050708]/40 cursor-pointer dark:hover:text-blue-400"
+                          : ""
+                      } 
                      rounded w-8 h-8 flex items-center justify-center`}
-                  >
-                    {typeof day === "number" ? day : null}
-                  </div>
-                );
-              })}
+                    >
+                      {typeof day === "number" ? day : null}
+                    </div>
+                  );
+                })}
+              </div>
             </div>
           </div>
           <div className="p-5 dark:bg-gray-700 bg-gray-50 rounded-b">
