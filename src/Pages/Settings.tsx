466cb6e46fc2c5c0a85b18f7e315d50b10b3ed4e
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2023-01-02 10:03:55 +0000

    Update 2 files

--- src/Pages/Settings.tsx
+++ src/Pages/Settings.tsx
@@ -1,9 +1,13 @@
 import Calendar from "../Components/Calendar";
+import CalendarEvents from "../Components/CalendarEvents";
 import EmojiComponent from "../Components/emojiComponent";
 import ThemeSwitcherBtn from "../Components/themeSwitcherBtn";
 const Settings = () => {
   return (
     <>
+    <aside>
+      <CalendarEvents />
+      </aside>
       <Calendar />
     </>
   );
