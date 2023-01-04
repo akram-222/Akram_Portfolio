5637c9e6f8c4133e0aba9ab6b48f1f87f80ed7df
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2023-01-04 11:10:40 +0000

    Update EventStateComponent.tsx

--- src/Components/EventStateComponent.tsx
+++ src/Components/EventStateComponent.tsx
@@ -12,9 +12,18 @@ const EventStateComponent = ({ eventsList, setEventsList }) => {
           key={i}
           className="flex rounded items-center p-1.5 dark:bg-[#171717] text-xs font-bold text-gray-900 bg-gray-50 group dark:text-white"
         >
-          <span className="flex-1 ml-3 whitespace-nowrap">{title}</span>
+          <span className="flex-1 ml-3 text-gray-300 whitespace-nowrap">{title}</span>
 
-          <span className="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">
+          <span
+            className={`
+          ${
+            priority === "Do first"
+              ? "text-red-400"
+              : priority === "Schedule"
+              ? "text-yellow-400"
+              : "text-green-400"
+          } inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium rounded`}
+          >
             {date}
           </span>
           <span
@@ -23,13 +32,13 @@ const EventStateComponent = ({ eventsList, setEventsList }) => {
               priority === "Do first"
                 ? "text-red-400"
                 : priority === "Schedule"
-                ? "text-blue-400"
-                : "text-yellow-400"
+                ? "text-yellow-400"
+                : "text-green-400"
             }
             inline-flex items-center pl-2`}
           >
             {priority === "Do first" ? (
-              <FcHighPriority  size={17}/>
+              <FcHighPriority size={17} />
             ) : priority === "Schedule" ? (
               <FcMediumPriority size={17} />
             ) : (
