46f7c8aeeb1bbb429bd5fae3bb6307b91ca0d0cf
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2023-01-07 20:18:01 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,18 +16,18 @@ import About from "./Pages/About";
 const App = () => {
   const [showSidebar, onSetShowSidebar] = useState(false);
   const [isLogging, setIsLogging] = useState(false);
-  useEffect(() => {
-    if (
-      localStorage.getItem("color-theme") === "dark" ||
-      (!("color-theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
-      document.documentElement.classList.add("dark");
-      localStorage.setItem("color-theme", "dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (
+  //     localStorage.getItem("color-theme") === "dark" ||
+  //     (!("color-theme" in localStorage) &&
+  //       window.matchMedia("(prefers-color-scheme: dark)").matches)
+  //   ) {
+  //     document.documentElement.classList.add("dark");
+  //     localStorage.setItem("color-theme", "dark");
+  //   } else {
+  //     document.documentElement.classList.remove("dark");
+  //   }
+  // }, []);
   return (
     <DevBlogsContextProvider>
       <div className="flex">
