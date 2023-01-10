cbc0871e16c7d96cc651fd073b4616548e4c4817
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2023-01-10 19:27:17 +0000

    Update PageTitle.tsx

--- src/Components/PageTitle.tsx
+++ src/Components/PageTitle.tsx
@@ -12,7 +12,7 @@ const PageTitle = ({
 }) => {
   return (
     <>
-      <div className={`${className} p-3 flex flex-col w-full justify-between`}>
+      <div className={`${className} py-3 px-2 flex flex-col w-full justify-between`}>
         <button
           onClick={onSidebarHide}
           type="button"
