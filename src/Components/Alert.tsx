222f8476086c6c299e7f2a3ad670fc0a668fc006
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2023-01-10 16:11:01 +0000

    Update Alert.tsx

--- src/Components/Alert.tsx
+++ src/Components/Alert.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { BsCircleFill, BsFillInfoCircleFill } from "react-icons/bs";
+import { TbSwitch2 } from "react-icons/tb";
 type alert = {
   msg: string;
   detailedMsg: string;
@@ -13,11 +15,12 @@ const Alert = ({
   setIsHidden,
   changeMode,
 }: alert) => {
+  const [isToggled, setToggled] = useState(true);
   return (
     <div
       className={`${
         isHidden ? "hidden" : ""
-      } animate-scaleUpCenter fixed top-2 max-w-[400px] right-2 p-4 mb-4 text-red-900 rounded-lg bg-gray-800 text-red-400`}
+      } animate-scaleUpCenter fixed max-w-[400px] right-2 bottom-2 p-4 mb-4 text-red-900 rounded-lg bg-gray-800 text-red-400`}
     >
       <div className="flex items-center">
         <BsFillInfoCircleFill className="mr-2" size={20} />
@@ -25,23 +28,33 @@ const Alert = ({
         <h3 className="text-lg font-medium">{msg}</h3>
       </div>
       <div className="mt-2 mb-4 text-sm text-gray-300">{detailedMsg}</div>
-      <div className="flex">
-        <button
-          type="button"
-          className="group text-white border border-gray-700 hover:border-green-400 hover:text-green-400 rounded text-xs px-3 py-1.5 mr-2 text-center inline-flex items-center"
-          onClick={() => changeMode()}
-        >
-          <BsCircleFill className="mr-1 group-hover:text-green-400" size={8} />
-          Recommended dark mode
-        </button>
-
-        <button
-          type="button"
-          className="border font-medium rounded text-xs px-3 py-1.5 text-center hover:bg-red-400 border-red-400 text-red-400 hover:text-white"
-          onClick={() => setIsHidden(true)}
-        >
-          Keep going to light mode
-        </button>
+      <div className="flex items-center">
+        <TbSwitch2
+          onClick={() => setToggled(!isToggled)}
+          className="cursor-pointer text-gray-700 mr-3 hover:text-gray-100"
+          size={23}
+        />
+        {isToggled ? (
+          <button
+            type="button"
+            className="group text-white border border-gray-700 hover:border-green-400 hover:text-green-400 rounded text-xs px-3 py-1.5 mr-2 text-center inline-flex items-center"
+            onClick={() => changeMode()}
+          >
+            <BsCircleFill
+              className="mr-1 group-hover:text-green-400"
+              size={8}
+            />
+            Recommended dark mode
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="border font-medium rounded text-xs px-3 py-1.5 text-center hover:bg-red-400 border-red-400 text-red-400 hover:text-white"
+            onClick={() => setIsHidden(true)}
+          >
+            Keep going to light mode
+          </button>
+        )}
       </div>
     </div>
   );
