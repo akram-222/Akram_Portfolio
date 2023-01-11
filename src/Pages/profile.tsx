c1a36cbd27e709780380f04589a54157e83b5414
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2023-01-11 13:47:27 +0000

    Update 3 files

--- src/Pages/profile.tsx
+++ src/Pages/profile.tsx
@@ -2,7 +2,9 @@ import FollowersList from "../Components/FollowersList";
 import {
   databasesAndCloudHoisting,
   frameworksAndLibraries,
-} from "../Utils/localeData/frameworksAndLibraries";
+  programmingAndMarkupLanguages,
+  softwaresAndTools,
+} from "../Utils/localeData/tools";
 const Profile = () => {
   return (
     <>
@@ -283,54 +285,9 @@ const Profile = () => {
           </em>
         </h3>
         <div className="flex gap-2 flex-wrap ml-8">
-          <a href="#">
-            <img
-              alt="CSS"
-              src="https://img.shields.io/badge/CSS-1572B6.svg?logo=css3&logoColor=white"
-            />
-          </a>
-          <a href="#">
-            <img
-              alt="HTML"
-              src="https://img.shields.io/badge/HTML-E34F26.svg?logo=html5&logoColor=white"
-            />
-          </a>
-          <a href="#">
-            <img
-              alt="JavaScript"
-              src="https://img.shields.io/badge/JavaScript-F7DF1E.svg?logo=javascript&logoColor=black"
-            />
-          </a>
-          <a href="#">
-            <img
-              alt="Python"
-              src="https://img.shields.io/badge/Python-14354C.svg?logo=python&logoColor=white"
-            />
-          </a>
-          <a href="#">
-            <img
-              alt="SASS"
-              src="https://img.shields.io/badge/Sass-hotpink.svg?logo=SASS&logoColor=white"
-            />
-          </a>
-          <a href="#">
-            <img
-              alt="SVG+XML"
-              src="https://img.shields.io/badge/SVG%2BXML-e0982c.svg?logo=svg&logoColor=white"
-            />
-          </a>
-          <a href="#">
-            <img
-              alt="Bash"
-              src="https://img.shields.io/badge/Bash-121011.svg?logo=gnu-bash&logoColor=white"
-            />
-          </a>
-          <a href="#">
-            <img
-              alt="Markdown"
-              src="https://img.shields.io/badge/Markdown-000000.svg?logo=markdown&logoColor=white"
-            />
-          </a>
+          {programmingAndMarkupLanguages?.map((item) => (
+            <img alt="frameworks and libraries" src={item} />
+          ))}
         </div>
         <h3 className="group my-4 text-lg text-white border-b inline-block border-gray-600/30">
           🧰 Frameworks and libraries
@@ -363,60 +320,9 @@ const Profile = () => {
           </em>
         </h3>
         <div className="ml-5 flex flex-wrap gap-2">
-          <a href="#">
-            <img
-              alt="Postman"
-              src="https://img.shields.io/badge/Postman-FF6C37?logo=postman&logoColor=white"
-            />
-          </a>
-          <a href="#">
-            <img
-              alt="Codepen"
-              src="https://img.shields.io/badge/Codepen-000000.svg?logo=codepen&logoColor=white"
-            />
-          </a>
-          <a href="#">
-            <img
-              alt="Discord"
-              src="https://img.shields.io/badge/-Discord-5865F2.svg?logo=discord&logoColor=white"
-            />
-          </a>
-          <a href="#">
-            <img
-              alt="Git"
-              src="https://img.shields.io/badge/Git-F05033.svg?logo=git&logoColor=white"
-            />
-          </a>
-          <a href="#">
-            <img
-              alt="GitHub Desktop"
-              src="https://img.shields.io/badge/GitHub%20Desktop-8034A9.svg?logo=github&logoColor=white"
-            />
-          </a>
-          <a href="#">
-            <img
-              alt="Google Sheets"
-              src="https://img.shields.io/badge/Google%20Sheets-34A853.svg?logo=google%20sheets&logoColor=white"
-            />
-          </a>
-          <a href="#">
-            <img
-              alt="Stack Overflow"
-              src="https://img.shields.io/badge/-Stack%20Overflow-FE7A16?logo=stack-overflow&logoColor=white"
-            />
-          </a>
-          <a href="#">
-            <img
-              alt="Visual Studio Code"
-              src="https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?logo=visual-studio-code&logoColor=white"
-            />
-          </a>
-          <a href="#">
-            <img
-              alt="WebStorm"
-              src="https://img.shields.io/badge/WebStorm-000000?logo=WebStorm&logoColor=white"
-            />
-          </a>
+          {softwaresAndTools?.map((item) => (
+            <img alt="frameworks and libraries" src={item} />
+          ))}
         </div>
       </div>
     </>
