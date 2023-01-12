2a2a3c7b689141aca0a4840f29154f7785541f74
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2023-01-12 10:23:36 +0000

    Update SearchComponent.tsx

--- src/Components/SearchComponent.tsx
+++ src/Components/SearchComponent.tsx
@@ -11,30 +11,33 @@ const SearchComponent = ({ list }) => {
   const [isHidden, setIsHidden] = useState<boolean>(true);
   const [isIdentical, setIsIdentical] = useState<boolean>(false);
   const inputFeild = useRef<HTMLInputElement | null>(null);
-
-  const filtering = (event: React.SyntheticEvent) => {
-    setIsHidden(false);
-    let input = (event.currentTarget as HTMLInputElement)!.value.toLowerCase();
-    let res = list.filter((item) => {
+  const checkIdenticality = (input) => {
+    filteredList.map((item) => {
       let itemName = item.name.toLowerCase();
       if (itemName === input) {
         setIsIdentical(true);
       } else {
         setIsIdentical(false);
       }
-
+    });
+  };
+  const filtering = (event: React.SyntheticEvent) => {
+    setIsHidden(false);
+    let input = (event.currentTarget as HTMLInputElement)!.value.toLowerCase();
+    let res = list.filter((item) => {
+      let itemName = item.name.toLowerCase();
       return itemName.includes(input);
     });
-
     setFilteredList(res);
+    checkIdenticality(input);
   };
-
+  useEffect(() => {}, [isIdentical]);
   return (
     <div className="relative">
       <input
         // onBlur={() => setIsHidden(true)}
         type={"search"}
-        onInput={(e) => filtering(e)}
+        onChange={(e) => filtering(e)}
         className="cursor-text h-9 flex items-center block px-2 py-1 overflow-hidden dark:text-white font-semibold w-[220px] border border-gray-300 rounded bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-transparent dark:border-gray-800 dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder="Search..."
         ref={inputFeild}
@@ -55,13 +58,15 @@ const SearchComponent = ({ list }) => {
                 size={8}
               />{" "}
               {item.name}
-              <span
-                className={`${
-                  isIdentical ? "" : "hidden"
-                } flex gap-1 text-green-400 inline-block ml-auto border-b border-b-gray-600/30 text-xs`}
-              >
-                <BsCheckCircleFill size={13} /> Identical
-              </span>
+              {isIdentical ? (
+                <span
+                  className={`flex gap-1 text-green-400 inline-block ml-auto border-b border-b-gray-600/30 text-xs`}
+                >
+                  <BsCheckCircleFill size={13} /> Identical
+                </span>
+              ) : (
+                ""
+              )}
             </li>
           ))
         ) : (
