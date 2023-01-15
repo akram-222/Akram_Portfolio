34206a06c853ab50933edbe889b2afd2a36db928
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2023-01-15 13:20:42 +0000

    Update 6 files

--- src/Components/ValidatorBtn.tsx
+++ src/Components/ValidatorBtn.tsx
@@ -1,4 +1,6 @@
 import { useCallback } from "react";
+import { app } from "../base";
+
 const ValidatorBtn = ({
   newGoal,
   goalsList,
@@ -9,6 +11,19 @@ const ValidatorBtn = ({
   const onClick = useCallback(() => {
     let uniqueGoals = new Set([...goalsList, newGoal.trim()]);
     setGoalsList([...uniqueGoals]);
+    ///////////////////
+    // const storageRef = app.storage().ref();
+    // let strData = JSON.stringify(goalsList);
+    // const fileRef = storageRef.child(strData);
+    // fileRef.put(strData).then(() => console.log("file Uploaded"));
+
+    const storageRef = app.storage().ref();
+    const fileRef = storageRef.child("[1,2,3]");
+
+    // Write the array
+    // storageRef.child(fileRef);
+
+    ///////////
     setNewGoal("");
   }, [newGoal]);
   return (
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/storage";
+import { getDatabase } from "firebase/database";
+// import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyCM8Nevx29oGcSfFZwUftIcmKDylRCzcB0",
+  authDomain: "akram-portfolio-b66de.firebaseapp.com",
+  projectId: "akram-portfolio-b66de",
+  storageBucket: "akram-portfolio-b66de.appspot.com",
+  messagingSenderId: "366480932294",
+  appId: "1:366480932294:web:520fd11a1109a8b0ab06c7",
+  measurementId: "G-3S8VYM0VJH",
+};
+
+// Initialize Firebase
+export const app = firebase.initializeApp(firebaseConfig);
+export const db = getDatabase(app);
