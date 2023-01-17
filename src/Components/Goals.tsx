cdf55a08edc5dc23ea34717fc58c2fb2334fd371
Akram Ashraf <akram.ashraf1026.sd@science.bsu.edu.eg>
2023-01-17 12:54:37 +0000

    Update Goals.tsx

--- src/Components/Goals.tsx
+++ src/Components/Goals.tsx
@@ -81,16 +81,12 @@ const Goals = () => {
       </div>
       <div className="flex flex-col overflow-auto px-3 text-white text-xl flex-grow">
         <div className="flex justify-between items-center">
-          <span>Goals List</span>
+          <span>{currentGoals ? "Completed" : "In Progress"} List</span>
           <button type="button" onClick={() => setCurrentGoals(+!currentGoals)}>
             {currentGoals ? (
-              <div className="text-xs flex gap-2">
-                <BsCardList className="text-green-500" size={20} />
-              </div>
+              <BsClockHistory className="text-yellow-500" size={20} />
             ) : (
-              <div className="text-xs flex gap-2">
-                <BsClockHistory className="text-yellow-500" size={20} />
-              </div>
+              <BsCardList className="text-green-500" size={20} />
             )}
           </button>
         </div>
