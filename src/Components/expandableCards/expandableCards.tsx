897b30fa26ae9fee5cdba58ed16e2fdb9db1e693
Akram Ashraf <69124951+Ak-ram@users.noreply.github.com>
2023-08-01 17:19:47 +0000

    Update 13 files

--- src/Components/expandableCards/expandableCards.tsx
+++ src/Components/expandableCards/expandableCards.tsx
@@ -1,11 +1,18 @@
 import "./expandableCards.css";
+import cp1 from "../../assests/cp1.png";
+import cp2 from "../../assests/cp2.png";
+import cp3 from "../../assests/cp3.png";
+import cp4 from "../../assests/cp4.png";
+import cp5 from "../../assests/cp5.png";
+import cp6 from "../../assests/cp6.png";
 const ExpandableCards = () => {
   return (
     <div className="expandableContainer h-auto">
       <div className="gallery">
         <div className="card">
           <img
-            src="https://user-images.githubusercontent.com/69124951/197331108-aff1dcef-1b6a-4c1a-96b7-ea95b00ec482.gif"
+            // src="https://user-images.githubusercontent.com/69124951/197331108-aff1dcef-1b6a-4c1a-96b7-ea95b00ec482.gif"
+            src={cp1}
             alt=""
           />
           <div className="info">
@@ -13,46 +20,31 @@ const ExpandableCards = () => {
           </div>
         </div>
         <div className="card">
-          <img
-            src="https://user-images.githubusercontent.com/69124951/197331573-90233082-d163-4d5c-bdeb-981ed62acf72.gif"
-            alt=""
-          />
+          <img src={cp2} alt="" />
           <div className="info">
             <h4 className="title">Space CSS Animation</h4>
           </div>
         </div>
         <div className="card">
-          <img
-            src="https://user-images.githubusercontent.com/69124951/197389695-6c30e4a0-e19a-44e1-b2b6-89f0116455c6.gif"
-            alt="Holy Grail Flexbox Layout"
-          />
+          <img src={cp3} alt="Holy Grail Flexbox Layout" />
           <div className="info">
             <h4 className="title">Holy Grail Flexbox Layout</h4>
           </div>
         </div>
         <div className="card">
-          <img
-            src="https://user-images.githubusercontent.com/69124951/197332571-059ad37c-d4a5-4c5f-a214-c6da19f1f8f2.gif"
-            alt="CSS Clock Animation"
-          />
+          <img src={cp4} alt="CSS Clock Animation" />
           <div className="info">
             <h4 className="title">CSS Clock Animation</h4>
           </div>
         </div>
         <div className="card">
-          <img
-            src="https://user-images.githubusercontent.com/69124951/197332978-1246b10e-0f0f-4dc4-9c3a-e9f3837efac0.gif"
-            alt="JS Tooltip Effect"
-          />
+          <img src={cp5} alt="JS Tooltip Effect" />
           <div className="info">
             <h4 className="title">JS Tooltip Effect</h4>
           </div>
         </div>
         <div className="card">
-          <img
-            src="https://user-images.githubusercontent.com/69124951/197283827-24c8ffa8-c3ca-4bf6-ad7e-14449fac5cf7.gif"
-            alt="css shadow animation"
-          />
+          <img src={cp6} alt="css shadow animation" />
           <div className="info">
             <h4 className="title">CSS Shadow Animation</h4>
           </div>
