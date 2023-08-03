f1e960f961b5c9e454392e546ecf90f26bc99bdf
Akram Ashraf <69124951+Ak-ram@users.noreply.github.com>
2023-08-03 10:11:23 +0000

    Update BreadCrumb.tsx

--- src/Components/BreadCrumb.tsx
+++ src/Components/BreadCrumb.tsx
@@ -1,15 +1,18 @@
-import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
+import { BsLayoutSidebarInset } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 const BreadCrumb = () => {
   const navigate = useNavigate();
   return (
-    <nav className="flex border border-gray-600/20 p-1 rounded hidden sm:block" aria-label="Breadcrumb">
+    <nav
+      className="flex border border-gray-600/20 p-1 rounded hidden sm:block"
+      aria-label="Breadcrumb"
+    >
       <ol className="inline-flex justify-between w-full items-center space-x-1 md:space-x-3">
         <li
           onClick={() => navigate(-1)}
           className="cursor-pointer inline-flex items-center font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
         >
-          <BiChevronLeft size={23} />
+          <BsLayoutSidebarInset size={23} />
           <span className="first-letter:uppercase">prev</span>
         </li>
         <li
@@ -17,7 +20,7 @@ const BreadCrumb = () => {
           className="cursor-pointer inline-flex items-center font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white"
         >
           <span className="first-letter:uppercase">next</span>
-          <BiChevronRight size={23} />
+          {/* <BiChevronRight size={23} /> */}
         </li>
       </ol>
     </nav>
