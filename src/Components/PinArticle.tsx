50806e2e42d89f7f94d9a9c37411ec4c29fe6b29
Akram Ashraf <69124951+Ak-ram@users.noreply.github.com>
2023-08-05 11:34:44 +0000

    Update PinArticle.tsx

--- src/Components/PinArticle.tsx
+++ src/Components/PinArticle.tsx
@@ -9,7 +9,7 @@ const PinArticle = ({
 }) => {
   return (
     <a
-      className={`${className} rounded-xl min-h-[100px] relative inline-block`}
+      className={`${className} overflow-hidden group rounded-xl min-h-[100px] relative inline-block`}
       // style={{
       //   backgroundImage: `url(${social_image})`,
       //   backgroundSize: "cover",
@@ -18,7 +18,7 @@ const PinArticle = ({
       href={url}
     >
       <div className="rounded-xl bg-gradient-to-tr from-[#101010] w-full h-full z-10"></div>
-      <div className="p-4 absolute bottom-0 left-0 w-full h-full z-20 flex flex-col justify-end">
+      <div className="p-4 absolute transition translate-y-full group-hover:translate-y-0 bottom-0 left-0 w-full h-full z-20 flex flex-col justify-end">
         <h2 className="font-semibold text-sm md:text-lg text-gray-100 leading-tight">
           {title}
         </h2>
