23182c07e88c99430ed07fdb85f585dc9c80483e
Akram Ashraf <69124951+Ak-ram@users.noreply.github.com>
2023-09-01 14:57:05 +0000

    Update BreadCrumb.tsx

--- src/Components/BreadCrumb.tsx
+++ src/Components/BreadCrumb.tsx
@@ -1,4 +1,4 @@
-import { TbLayoutSidebarLeftExpand,TbBrandLinkedin,TbLayoutSidebarRightExpand,TbBrandTwitter } from "react-icons/tb";
+import { TbLayoutSidebarLeftExpand,TbBrandLinkedin,TbLayoutSidebarRightExpand,TbBrandTwitter,TbBrandFacebook } from "react-icons/tb";
 import {useState,useContext} from 'react';
 import {useLocation} from "react-router-dom";
 import SocialShareButton from "./socialShareButton"
@@ -20,18 +20,22 @@ const BreadCrumb = () => {
         <li onClick={toggleSidebar} className="cursor-pointer inline-flex items-center font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
           {isSidebarOpen ?<TbLayoutSidebarRightExpand size={23} />:<TbLayoutSidebarLeftExpand size={23} />}
         </li>
-        <li className="dark:text-gray-400 text-sm font-bold flex items-center border-l-2 pl-2">
+        <li className="mr-2 dark:text-gray-400 text-sm font-bold flex items-center border-l-2 pl-2">
 Share this page on         </li>
-        <li className="cursor-pointer inline-flex font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+        <li className="ml-0 cursor-pointer inline-flex font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
        <SocialShareButton url={currentRoute} platform='https://twitter.com/intent/tweet?url='>
         <TbBrandTwitter size="25"/>
         </SocialShareButton>
     </li>
-    <li className="cursor-pointer inline-flex font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
-        <SocialShareButton url={currentRoute} platform='https://twitter.com/intent/tweet?url='>
+    <li className="ml-0 cursor-pointer inline-flex font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+        <SocialShareButton url={currentRoute} platform='https://www.linkedin.com/shareArticle?mini=true&url='>
         <TbBrandLinkedin size="25"/>
         </SocialShareButton>
- 
+    </li>
+    <li className="ml-0 cursor-pointer inline-flex font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+        <SocialShareButton url={currentRoute} platform='https://www.facebook.com/sharer/sharer.php?u='>
+        <TbBrandFacebook size="23"/>
+        </SocialShareButton>
     </li>
       </ol>
     </nav>
