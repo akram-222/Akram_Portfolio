c335f6a1ba3dee63453857806df9b1c702fe4e6a
Akram Ashraf <69124951+Ak-ram@users.noreply.github.com>
2023-09-01 14:24:17 +0000

    Update BreadCrumb.tsx

--- src/Components/BreadCrumb.tsx
+++ src/Components/BreadCrumb.tsx
@@ -1,4 +1,4 @@
-import { TbLayoutSidebarLeftExpand,TbLayoutSidebarRightExpand,TbBrandTwitter } from "react-icons/tb";
+import { TbLayoutSidebarLeftExpand,TbBrandLinkedin,TbLayoutSidebarRightExpand,TbBrandTwitter } from "react-icons/tb";
 import {useState,useContext} from 'react';
 import {useLocation} from "react-router-dom";
 import SocialShareButton from "./socialShareButton"
@@ -16,16 +16,22 @@ const BreadCrumb = () => {
       className="flex border border-gray-600/50 mb-3 py-1 px-3 rounded hidden sm:block"
       aria-label="Breadcrumb"
     >
-      <ol className="flex sticky top-0 p-1 gap-2 w-full items-self-start space-x-1 md:space-x-3">
+      <ol className="flex sticky top-0 p-1 gap-2 w-full space-x-1 md:space-x-3">
         <li onClick={toggleSidebar} className="cursor-pointer inline-flex items-center font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
           {isSidebarOpen ?<TbLayoutSidebarRightExpand size={23} />:<TbLayoutSidebarLeftExpand size={23} />}
         </li>
-        <li className="cursor-pointer inline-flex items-center font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+        <li className="dark:text-gray-400 text-sm font-bold flex items-center border-l-2 pl-2">
+Share this page on         </li>
+        <li className="cursor-pointer inline-flex font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
        <SocialShareButton url={currentRoute} platform='https://twitter.com/intent/tweet?url='>
-        <TbBrandTwitter size="23"/>
+        <TbBrandTwitter size="25"/>
         </SocialShareButton>
-   
-   
+    </li>
+    <li className="cursor-pointer inline-flex font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+        <SocialShareButton url={currentRoute} platform='https://twitter.com/intent/tweet?url='>
+        <TbBrandLinkedin size="25"/>
+        </SocialShareButton>
+ 
     </li>
       </ol>
     </nav>
