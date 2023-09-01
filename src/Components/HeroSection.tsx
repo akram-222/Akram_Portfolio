8b27883271404fbcd3971ed7b037deac21532bef
Akram Ashraf <69124951+Ak-ram@users.noreply.github.com>
2023-09-01 02:46:52 +0000

    Update HeroSection.tsx

--- src/Components/HeroSection.tsx
+++ src/Components/HeroSection.tsx
@@ -30,13 +30,12 @@ const HeroSection = () => {
             </span>
           </h1>
           <p className="mb-8 sm:text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-            We concentrate on frontend technology issues across a variety of
-            technologies here at Our Blog.{" "}
+           At Our Blog, we cover a wide range of frontend technology issues.{" "}
           </p>
           <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
             <a
               href="#d"
-              className="w-fit m-auto inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-xs sm:text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white "
+              className="z-10 w-fit m-auto inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-xs sm:text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white "
               role="alert"
             >
               <span className="bg-blue-600 rounded-full text-white px-4 py-1.5 mr-3">
@@ -44,9 +43,9 @@ const HeroSection = () => {
               </span>{" "}
               <a
                 href="https://dev.to/ak_ram"
-                className="group flex items-center dark:text-blue-400 dark:hover:underline font-medium"
+                className="cursor-pointer group flex items-center dark:text-blue-400 dark:hover:underline font-medium"
               >
-                Interested ? See what's new
+               Check out our latest posts.
                 <BiChevronRight
                   className={"text-white transition group-hover:translate-x-1"}
                   size={20}
