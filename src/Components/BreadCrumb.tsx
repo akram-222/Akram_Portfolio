70c2152777871ac71967a63cb3629877a4691128
Akram Ashraf <69124951+Ak-ram@users.noreply.github.com>
2023-09-01 13:00:01 +0000

    Update 3 files

--- src/Components/BreadCrumb.tsx
+++ src/Components/BreadCrumb.tsx
@@ -1,7 +1,8 @@
 import { TbLayoutSidebarLeftExpand,TbLayoutSidebarRightExpand } from "react-icons/tb";
 import {useState,useContext} from 'react';
 import {SidebarContext } from "../Contexts/sidebarContext";
-import ShareButton from 'react-share';
+import {ShareSocial} from 'react-share-social' 
+
 const BreadCrumb = () => {
  
   const { isSidebarOpen, toggleSidebar } = useContext(SidebarContext);
@@ -12,15 +13,26 @@ const BreadCrumb = () => {
       className="flex border border-gray-600/50 mb-3 py-1 px-3 rounded hidden sm:block"
       aria-label="Breadcrumb"
     >
-      <ol className="flex sticky top-0 p-1 justify-between w-full items-center space-x-1 md:space-x-3">
+      <ol className="flex sticky top-0 p-1 gap-2 w-full items-center space-x-1 md:space-x-3">
         <li onClick={toggleSidebar} className="cursor-pointer inline-flex items-center font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
           {isSidebarOpen ?<TbLayoutSidebarRightExpand size={23} />:<TbLayoutSidebarLeftExpand size={23} />}
         </li>
-        <li onClick={toggleSidebar} className="cursor-pointer inline-flex items-center font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
-         <ShareButton
+        <li className="cursor-pointer inline-flex items-center font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+        <ShareSocial 
+     url ="url_to_share.com"
+     socialTypes={['facebook','twitter',]}
+   />
+   <ShareSocial 
+     url ="url_to_share.com"
+     socialTypes={['linkedin']}
+   />
+   <SocialShare
       network="twitter"
-      url={"https://twitter.com"}
+      url={window.location.href}
       title="Share this article on Twitter"
+      urlParams={{
+        text: window.document.querySelector('h1').textContent.slice(0, 100),
+      }}
     />
     </li>
       </ol>
