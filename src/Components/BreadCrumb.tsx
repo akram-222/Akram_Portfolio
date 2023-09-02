08097505c725d3f09c3eb7b4639e26bf855b23c7
Akram Ashraf <69124951+Ak-ram@users.noreply.github.com>
2023-09-02 20:34:06 +0000

    Update BreadCrumb.tsx

--- src/Components/BreadCrumb.tsx
+++ src/Components/BreadCrumb.tsx
@@ -13,11 +13,11 @@ const BreadCrumb = () => {
   return (
     
     <nav
-      className="flex border border-gray-600/50 mb-3 py-1 px-3 rounded hidden sm:block"
+      className="flex border border-gray-600/50 mb-3 py-1 px-3 rounded sm:block"
       aria-label="Breadcrumb"
     >
       <ol className="flex sticky top-0 p-1 gap-2 w-full space-x-1 md:space-x-3">
-        <li onClick={toggleSidebar} className="cursor-pointer inline-flex items-center font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
+        <li onClick={toggleSidebar} className="cursor-pointer hidden sm:inline-flex items-center font-medium text-gray-700 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white">
           {isSidebarOpen ?<TbLayoutSidebarRightExpand size={23} />:<TbLayoutSidebarLeftExpand size={23} />}
         </li>
         <li className="mr-2 dark:text-gray-400 text-sm font-bold flex items-center border-l-2 pl-2">
